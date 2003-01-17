2e587069a4e6fa059ed5b86fcdd4f659f93b4364
theller <none@none>
2003-01-17 20:03:57 +0000

    Test the reimplementation of the SimpleType_from_param classmethod.
    --HG--
    branch : ctypes
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk/ctypes%4047299

--- unittests/test_parameters.py
+++ unittests/test_parameters.py
@@ -1,6 +1,8 @@
 #
 # unit tests for converting parameters to function calls.
 #
+
+#
 # When you assign an argtypes sequence to a function loaded from a dll,
 # ctypes internally looks for a .from_param() classmethod on the sequence
 # items and stores it as a converter.
@@ -17,41 +19,73 @@
 # The second element is either an integer (for the 'c', 'i' codes),
 # 
 
+#
+# What is the _as_parameter_ property?
+#
+# It is a read-only property, used when an instance of c_int, c_float
+# and so on is passed to a function call.
+#
+# The _as_parameter_ property 'returns' a 3-tuple as described above,
+# or a Python integer.
+
+
 def test_float():
+    # C floats are created from Python integers, longs, and floats.
     """
     >>> from ctypes import c_float
 
     >>> c_float.from_param(2)
     ('f', 2.0, None)
 
-    >>> # Hm, this *should* work...
-    >>> c_float.from_param(2000000000000000L)
-    Traceback (most recent call last):
-      ...
-    TypeError: expected number instead of long instance
-
     >>> c_float.from_param(-2.0)
     ('f', -2.0, None)
+
+    >>> c_float.from_param(10000000000L)
+    ('f', 10000000000.0, None)
+
+    >>> c_float.from_param(-10000000000L)
+    ('f', -10000000000.0, None)
+
+    >>> c_float.from_param(c_float(-2.0))
+    c_float(-2.000000)
+    
+    >>> c_float(2.0)._as_parameter_
+    ('f', 2.0, c_float(2.000000))
+
+    >>> c_float(2.0)._as_parameter_ = None
+    Traceback (most recent call last):
+    TypeError: attribute '_as_parameter_' of '_ctypes._SimpleCData' objects is not writable
+    
     """
 
 
 def test_double():
+    # C doubles are created from Python integers, longs, and floats.
     """
     >>> from ctypes import c_double
 
     >>> c_double.from_param(2)
     ('d', 2.0, None)
 
-    >>> c_double.from_param(2000000000000000L)
-    Traceback (most recent call last):
-      ...
-    TypeError: expected number instead of long instance
-
     >>> c_double.from_param(-2.0)
     ('d', -2.0, None)
+
+    >>> c_double.from_param(10000000000L)
+    ('d', 10000000000.0, None)
+
+    >>> c_double.from_param(-10000000000L)
+    ('d', -10000000000.0, None)
+
+    >>> c_double.from_param(c_double(-2.0))
+    c_double(-2.000000)
+
+    >>> c_double(2.0)._as_parameter_
+    ('d', 2.0, c_double(2.000000))
+    
     """
 
 def test_integer():
+    # C integers are created from Python integers and longs.
     """
     >>> from ctypes import c_byte, c_ubyte, c_short, c_ushort
     >>> from ctypes import c_int, c_uint, c_long, c_ulong
@@ -72,21 +106,101 @@ def test_integer():
 
     >>> [t.from_param(t(10)) for t in unsigned_types]
     [c_ubyte(10), c_ushort(10), c_uint(10), c_ulong(10)]
+
+    >>> c_int(42)._as_parameter_
+    ('i', 42, c_int(42))
     """
 
 def test_strings():
+    # C char * is created from Python strings,
+    # or from None to create a NULL pointer
     """
     >>> from ctypes import c_string
     >>> c_string.from_param("123")
     '123'
 
+    >>> c_string.from_param(u"123")
+    Traceback (most recent call last):
+       ...
+    TypeError: c_string, string, or None expected
+
     >>> c_string.from_param(c_string("123"))
     <c_string '123'>
 
-    >>> # NULL pointers can be specified by using None:
     >>> c_string.from_param(None)
     0
 
+    # this returns the addres of the internal string buffer
+    >>> type(c_string("abc")._as_parameter_)
+    <type 'int'>
+
+    >>> c_string(None)
+    <c_string NULL>
+    
+    >>> c_string(None)._as_parameter_
+    0
+    """
+
+# disabled, doesn't yet work: c_wstring has no from_param classmethod.
+
+##def test_wstrings():
+##    # C wchar * is created from Python strings,
+##    # or from None to create a NULL pointer
+##    """
+##    >>> from ctypes import c_wstring
+##    >>> c_wstring.from_param("123")
+##    '123'
+
+##    >>> c_string.from_param(u"123")
+##    Traceback (most recent call last):
+##       ...
+##    TypeError: c_string, string, or None expected
+
+##    >>> c_string.from_param(c_string("123"))
+##    <c_string '123'>
+
+##    >>> c_string.from_param(None)
+##    0
+##    """
+
+def test_char():
+    """
+    >>> from ctypes import c_char
+    >>> c_char.from_param("x")
+    120
+
+    >>> c_char(42)
+    Traceback (most recent call last):
+        ...
+    TypeError: one character string expected
+
+    >>> c_char("abc")
+    Traceback (most recent call last):
+        ...
+    TypeError: one character string expected
+
+    >>> c_char.from_param(u"x")
+    Traceback (most recent call last):
+        ...
+    TypeError: expected one character string instead of unicode instance
+
+    >>> c_char.from_param(c_char("x"))
+    c_char('x')
+
+    >>> c_char("x")._as_parameter_
+    ('c', 120, c_char('x'))
+
+    """
+
+def test_longlong():
+    # currently you cannot pass c_longlong and c_ulonglong values
+    # to function calls.
+    """
+    >>> from ctypes import c_longlong
+    >>> c_longlong.from_param(42)
+    Traceback (most recent call last):
+       ...
+    TypeError: c_longlong and c_ulonglong objects cannot be passed as function parameters
     """
 
 def test(*args, **kw):
