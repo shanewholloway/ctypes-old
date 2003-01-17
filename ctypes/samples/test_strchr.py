28085ad6c1e994d55274110bb3ae3b7e80e02207
theller <none@none>
2003-01-17 14:07:36 +0000

    Imported old sample scripts, a new README.txt explaining the samples directory.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4047294

---
+++ ctypes/samples/test_strchr.py
@@ -0,0 +1,18 @@
+from ctypes import c_char, c_string, byref, cdll, c_int
+
+import os
+if os.name == "nt":
+    strchr = cdll.msvcrt.strchr
+elif os.name == "posix":
+    strchr = cdll.LoadLibrary("/lib/libc.so.6").strchr
+
+strchr.restype = "s"
+
+strchr.argtypes = [c_string, c_int]
+
+assert "def" ==  strchr("abcdef", ord("d"))
+assert None == strchr("abcdef", ord("x"))
+
+strchr.argtypes = [c_string, c_char]
+assert None == strchr("abcdef", "x")
+assert "def" == strchr("abcdef", "d")
