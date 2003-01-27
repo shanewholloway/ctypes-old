2624a23c038e1ff6bc1a4383b03bf5feac6ceb52
theller <none@none>
2003-01-27 13:53:59 +0000

    Simple minded test or leaks.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4047378

---
+++ ctypes/unittests/test_refcounts.py
@@ -0,0 +1,43 @@
+import unittest
+import ctypes
+
+class MyCallback(ctypes.CFunction):
+    _stdcall_ = 0
+    _types_ = "i"
+
+
+class RefcountTestCase(unittest.TestCase):
+    def setUp(self):
+        global dll
+        import _ctypes
+        dll = ctypes.CDLL(_ctypes.__file__)
+
+    def test_1(self):
+        from sys import getrefcount as grc
+
+        f = dll._testfunc_callback_i_if
+        f.restype = "i"
+        f.argtypes = [ctypes.c_int, MyCallback]
+
+        def callback(value):
+            #print "called back with", value
+            return value
+
+        self.failUnless(grc(callback) == 2)
+        cb = MyCallback(callback)
+        self.failUnless(grc(callback) == 3)
+        result = f(-10, cb)
+        self.failUnless(result == -18)
+        cb = None
+        self.failUnless(grc(callback) == 2)
+
+
+def get_suite():
+    return unittest.makeSuite(RefcountTestCase)
+
+def test(verbose=0):
+    runner = unittest.TextTestRunner(verbosity=verbose)
+    runner.run(get_suite())
+
+if __name__ == '__main__':
+    unittest.main()
