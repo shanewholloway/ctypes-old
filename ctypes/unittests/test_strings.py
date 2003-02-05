a14cd2a9d431dd9ab0c7e0d457df286b10c4ba4b
theller <none@none>
2003-02-05 12:24:30 +0000

    Wrote some real tests, which also check for the bugs fixed recently.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4047399

--- ctypes/unittests/test_strings.py
+++ ctypes/unittests/test_strings.py
@@ -1,13 +1,79 @@
 import unittest
 
 class StringTestCase(unittest.TestCase):
-    pass
+    def test_basic_strings(self):
+        from ctypes import c_string
+        cs = c_string("abcdef")
+
+        # XXX Thsi behaviour is about to change:
+        # len returns the size of the internal buffer in bytes.
+        # This includes the terminating NUL character.
+        self.failUnless(len(cs) == 7)
+
+        # The value property is the string up to the first terminating NUL.
+        self.failUnless(cs.value == "abcdef")
+        self.failUnless(c_string("abc\000def").value == "abc")
+
+        # The raw property is the total buffer contents:
+        self.failUnless(cs.raw == "abcdef\000")
+        self.failUnless(c_string("abc\000def").raw == "abc\000def\000")
+
+        # We can change the value:
+        cs.value = "ab"
+        self.failUnless(cs.value == "ab")
+        self.failUnless(cs.raw == "ab\000\000\000\000\000")
+
+        cs.raw = "XY"
+        self.failUnless(cs.value == "XY")
+        self.failUnless(cs.raw == "XY\000\000\000\000\000")
+
+    def test_toolong(self):
+        from ctypes import c_string
+        cs = c_string("abcdef")
+        # Much too long string:
+        self.assertRaises(ValueError, setattr, cs, "value", "123456789012345")
+
+        # One char too long values:
+        self.assertRaises(ValueError, setattr, cs, "value", "1234567")
 
 ##    def test_perf(self):
 ##        check_perf()
-        
-from ctypes import c_string
 
+class UNUSED:
+    def test_basic_wstrings(self):
+        from ctypes import c_wstring
+        cs = c_wstring(u"abcdef")
+
+        # XXX Thsi behaviour is about to change:
+        # len returns the size of the internal buffer in bytes.
+        # This includes the terminating NUL character.
+        self.failUnless(len(cs) == 14)
+
+        # The value property is the string up to the first terminating NUL.
+        print repr(cs.value)
+        self.failUnless(cs.value == u"abcdef")
+        self.failUnless(c_string("abc\000def").value == "abc")
+
+        # The raw property is the total buffer contents:
+        self.failUnless(cs.raw == "abcdef\000")
+        self.failUnless(c_string("abc\000def").raw == "abc\000def\000")
+
+        # We can change the value:
+        cs.value = "ab"
+        self.failUnless(cs.value == "ab")
+        self.failUnless(cs.raw == "ab\000\000\000\000\000")
+
+
+    def test_toolong(self):
+        from ctypes import c_string
+        cs = c_string("abcdef")
+        # Much too long string:
+        self.assertRaises(ValueError, setattr, cs, "value", "123456789012345")
+
+        # One char too long values:
+        self.assertRaises(ValueError, setattr, cs, "value", "1234567")
+
+        
 def run_test(rep, msg, func, arg):
     items = range(rep)
     from time import clock
@@ -19,7 +85,7 @@ def run_test(rep, msg, func, arg):
 
 def check_perf():
     # Construct 5 objects
-    from ctypes import c_int
+    from ctypes import c_string
 
     REP = 200000
 
@@ -29,14 +95,15 @@ def check_perf():
 # Python 2.3 -OO, win2k, P4 700 MHz:
 #
 #      c_string(None): 1.75 us
-#     c_string('abc'): 2.69 us
+#     c_string('abc'): 2.74 us
 
 # Python 2.2 -OO, win2k, P4 700 MHz:
 #
-#      c_string(None): 2.85 us
-#     c_string('abc'): 3.56 us
+#      c_string(None): 2.95 us
+#     c_string('abc'): 3.67 us
 
 def get_suite():
+    import unittest
     return unittest.makeSuite(StringTestCase)
 
 def test(verbose=0):
