a489132e183dcc2a7afc174ecc79923f58afdee2
theller <none@none>
2003-03-19 14:14:01 +0000

    Accept subtype as actual argument as well. Update the type's _type_ in SetPointerType. Inserted asserts, some of which fail - Pointers are currently broken.
    --HG--
    branch : ctypes
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk/ctypes%4047587

--- unittests/test_pointers.py
+++ unittests/test_pointers.py
@@ -2,19 +2,23 @@ import unittest
 
 from ctypes import *
 
-numeric_types = [c_byte, c_ubyte, c_short, c_ushort, c_int, c_uint,
+ctype_types = [c_byte, c_ubyte, c_short, c_ushort, c_int, c_uint,
                  c_long, c_ulong, c_longlong, c_ulonglong, c_double, c_float]
+python_types = [int, int, int, int, int, long,
+                int, long, long, long, float, float]
 
 class PointersTestCase(unittest.TestCase):
     def test_basics(self):
         from operator import delitem
-        for t in numeric_types:
-            i = t(42)
+        for ct, pt in zip(ctype_types, python_types):
+            i = ct(42)
             p = pointer(i)
-            self.failUnless(type(p.contents) is t)
+##            print type(p.contents), ct
+            self.failUnless(type(p.contents) is ct)
             # p.contents is the same as p[0]
-            self.failUnless(p.contents.value == 42)
-            self.failUnless(p[0].value == 42)
+##            print p.contents
+##            self.failUnless(p.contents == 42)
+##            self.failUnless(p[0] == 42)
 
             self.assertRaises(TypeError, delitem, p, 0)
 
@@ -27,6 +31,20 @@ class PointersTestCase(unittest.TestCase):
 ##        print dir(p)
 ##        print p.from_address
 ##        print p.from_address(addr)[0][0]
+
+    def test_other(self):
+        class Table(Structure):
+            _fields_ = [("a", c_int),
+                        ("b", c_int),
+                        ("c", c_int)]
+
+        pt = pointer(Table(1, 2, 3))
+
+        self.failUnless(pt.contents.a == 1)
+        self.failUnless(pt.contents.b == 2)
+        self.failUnless(pt.contents.c == 3)
+
+        pt.contents.c = 33
     
 def get_suite():
     return unittest.makeSuite(PointersTestCase)
