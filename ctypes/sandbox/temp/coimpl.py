13109a4d9b07e82fa3402a27e912a5f53d84f924
theller <none@none>
2003-03-20 15:09:29 +0000

    Lazy creation of Interface class attributes.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4047598

--- ctypes/sandbox/temp/coimpl.py
+++ ctypes/sandbox/temp/coimpl.py
@@ -1,6 +1,7 @@
 from ctypes import Structure, c_voidp, c_int, c_ulong, oledll, c_uint, POINTER, WinError
 from ctypes import pointer, Union, byref, STDAPI
 from ctypes.com import GUID, REFIID
+from ctypes import SetPointerType
 
 ole32 = oledll.ole32
 
@@ -20,29 +21,47 @@ def STDMETHOD(restype, name, *argtypes):
 
 class _interface_meta(type(Structure)):
     def __new__(self, name, bases, kwds):
-        class _VTable(Structure):
-            _fields_ = kwds["_methods_"]
-        fields = [("lpVtbl", POINTER(_VTable))]
+        self.VTable_ptr = POINTER("%s_VTable" % name)
+        fields = [("lpVtbl", self.VTable_ptr)]
         kwds["_fields_"] = fields
         return type(Structure).__new__(self, name, bases, kwds)
 
+    def make_vtable(self):
+        class _VTable(Structure):
+            _fields_ = self._methods_
+        SetPointerType(self.VTable_ptr, _VTable)
+
+    def __call__(self, *args):
+        print "__call__"
+        result = type(Structure).__call__(self, *args)
+        if not hasattr(self, "AddRef"):
+            self.make_vtable()
+            self.make_methods()
+        return result
+
+    def make_methods(self):
+        print "make client methods for interface", self.__name__
+        import new
+        index = 0
+        for name, PROTO in self._fields_[0][1]._type_._fields_: # VTable._fields_
+            print "\t%d %s" % (index, name)
+            clientPROTO = STDAPI(PROTO._restype_, *PROTO._argtypes_[1:])
+            mth = new.instancemethod(clientPROTO(index), None, self)
+            setattr(self, name, mth)
+            index += 1
+
+print "# creating IUnknown"
 class IUnknown(Structure):
     __metaclass__ = _interface_meta
-    _methods_ = [STDMETHOD(c_int, "QueryInterface", REFIID, POINTER(PIUnknown)),
-                 STDMETHOD(c_ulong, "AddRef"),
-                 STDMETHOD(c_ulong, "Release")]
+
+
+IUnknown._methods_ = [STDMETHOD(c_int, "QueryInterface", REFIID, POINTER(PIUnknown)),
+                      STDMETHOD(c_ulong, "AddRef"),
+                      STDMETHOD(c_ulong, "Release")]
+print "# done IUnknown"
+print
     
-from ctypes import SetPointerType
 SetPointerType(PIUnknown, IUnknown)
-
-import new
-
-index = 0
-for name, PROTO in IUnknown._fields_[0][1]._type_._fields_: # VTable._fields_
-    clientPROTO = STDAPI(PROTO._restype_, *PROTO._argtypes_[1:])
-    mth = new.instancemethod(clientPROTO(index), None, IUnknown)
-    setattr(IUnknown, name, mth)
-    index += 1
 
 ################################################################
 # implementing a COM interface pointer
@@ -65,13 +84,13 @@ class COClass:
         return self._refcnt
 
     def make_interface_pointer(self, itfclass):
+        itf = itfclass()
         vtbltype = itfclass._fields_[0][1]._type_
         methods = []
         for name, proto in vtbltype._fields_:
             callable = getattr(self, name, self._notimpl)
             methods.append(proto(callable))
         vtbl = vtbltype(*methods)
-        itf = itfclass()
         itf.lpVtbl = pointer(vtbl)
         return itf
 
@@ -82,7 +101,13 @@ dll = CDLL(_ctypes.__file__)
 
 ob = COClass()
 
+print "# make interface pointer"
 itf = ob.make_interface_pointer(IUnknown)
+print "# done make interface pointer"
+
+print "# make interface pointer"
+itf = ob.make_interface_pointer(IUnknown)
+print "# done make interface pointer"
 
 print "From Python"
 
