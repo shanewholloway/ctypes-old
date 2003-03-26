c05c03cc542d3d0534e238457a83ea1462f04f53
theller <none@none>
2003-03-26 20:44:23 +0000

    Some more tests for pointers. Pointers are very confusing.
    --HG--
    branch : ctypes
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk/ctypes%4047636

--- unittests/test_pointers.py
+++ unittests/test_pointers.py
@@ -8,6 +8,74 @@ python_types = [int, int, int, int, int, long,
                 int, long, long, long, float, float]
 
 class PointersTestCase(unittest.TestCase):
+    def test_pass_pointers(self):
+        import _ctypes
+        dll = CDLL(_ctypes.__file__)
+        func = dll._testfunc_p_p
+
+        i = c_int(12345678)
+##        func.argtypes = (POINTER(c_int),)
+        address = func(byref(i))
+        self.failUnless(c_int.from_address(address).value == 12345678)
+
+        func.restype = POINTER(c_int)
+        res = func(pointer(i))
+        self.failUnless(res.contents.value == 12345678)
+        self.failUnless(res[0] == 12345678)
+
+    def test_change_pointers(self):
+        import _ctypes
+        dll = CDLL(_ctypes.__file__)
+        func = dll._testfunc_p_p
+
+        i = c_int(87654)
+        func.restype = POINTER(c_int)
+        func.argtypes = (POINTER(c_int),)
+
+        res = func(pointer(i))
+        self.failUnless(res[0] == 87654)
+        self.failUnless(res.contents.value == 87654)
+
+        # C code: *res = 54345
+        res[0] = 54345
+        self.failUnless(i.value == 54345)
+
+        # C code:
+        #   int x = 12321;
+        #   res = &x
+        res.contents = c_int(12321)
+        self.failUnless(i.value == 54345)
+
+    def test_callbacks_with_pointers(self):
+        # a function type receiving a pointer
+        PROTOTYPE = CALLBACK(c_int, POINTER(c_int))
+
+        self.result = []
+
+        def func(arg):
+            for i in range(10):
+##                print arg[i],
+                self.result.append(arg[i])
+            print
+            return 0
+        callback = PROTOTYPE(func)
+
+        import _ctypes
+        dll = CDLL(_ctypes.__file__)
+        # This function expects a function pointer,
+        # and calls this with an integer pointer as parameter.
+        # The int pointer points to a table containing the numbers 1..10
+        doit = dll._testfunc_callback_with_pointer
+
+##        i = c_int(42)
+##        callback(byref(i))
+##        self.failUnless(i.value == 84)
+
+        doit(callback)
+        print self.result
+        doit(callback)
+        print self.result
+        
     def test_basics(self):
         from operator import delitem
         for ct, pt in zip(ctype_types, python_types):
