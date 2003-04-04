f03e38cf9fbaf1e97283e3070e691b03303b8d71
theller <none@none>
2003-04-04 14:33:40 +0000

    Replace the ctypes' c_string type with a *function* returning a c_char array.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4047653

--- ctypes/samples/Windows/test_enumwindows.py
+++ ctypes/samples/Windows/test_enumwindows.py
@@ -1,8 +1,26 @@
 # from Paul Moore via comp.lang.python
-from ctypes import windll, c_string, c_int, WinFuncType
+from ctypes import windll, c_int, WinFuncType, c_char
 user32 = windll.user32
 
 EnumWindowsProc = WinFuncType(c_int, c_int, c_int)
+
+def c_string(init, size=None):
+    """c_string(aString) -> character array
+    c_string(anInteger) -> character array
+    c_string(aString, anInteger) -> character array
+    """
+    if isinstance(init, str):
+        if size is None:
+            size = len(init)+1
+        buftype = c_char * size
+        buf = buftype()
+        buf.value = init
+        return buf
+    elif isinstance(init, int):
+        buftype = c_char * init
+        buf = buftype()
+        return buf
+    raise TypeError, init
 
 def DisplayWindow(hwnd, lparam):
     title = c_string('\000' * 256)
