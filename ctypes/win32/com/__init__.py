4a894beb983d231bebf6046d0177eaab95591a39
theller <none@none>
2003-04-10 16:15:40 +0000

    Forgot to import c_wchar.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4047664

--- ctypes/win32/com/__init__.py
+++ ctypes/win32/com/__init__.py
@@ -1,5 +1,5 @@
 from ctypes import Structure, POINTER, c_voidp, c_ubyte, c_byte, c_int, \
-     c_ushort, c_short, c_uint, c_long, c_ulong, c_wchar_p, \
+     c_ushort, c_short, c_uint, c_long, c_ulong, c_wchar_p, c_wchar, \
      oledll, byref, sizeof, WINFUNCTYPE, SetPointerType, WinError, pointer
 
 from _ctypes import HRESULT
