6d7187524368f77771e09a50c7693237a396a3a7
theller <none@none>
2003-04-10 16:01:33 +0000

    Base class methods were missing, and removed the unneeded xxxPointer classes.
    --HG--
    branch : ctypes
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk/ctypes%4047663

--- win32/com/ole.py
+++ win32/com/ole.py
@@ -1,44 +1,32 @@
-from ctcom import IUnknown, STDMETHOD, HRESULT, GUID, COMPointer
+from ctypes.com import IUnknown, STDMETHOD, HRESULT, GUID
 
 from ctypes import c_int, POINTER, Structure
-from windows import MSG, RECT, OLESTR
+from ctypes.wintypes import MSG, RECT, OLESTR
 
 BORDERWIDTHS = RECT
 
 class IOleWindow(IUnknown):
     _iid_ = GUID("{00000114-0000-0000-C000-000000000046}")
 
-class IOleWindowPointer(COMPointer):
-    _interface_ = IOleWindow
-
-
 class IOleInPlaceUIWindow(IOleWindow):
     _iid_ = GUID("{00000115-0000-0000-C000-000000000046}")
-
-class IOleInPlaceUIWindowPointer(COMPointer):
-    _interface_ = IOleInPlaceUIWindow
-
 
 class IOleInPlaceActiveObject(IOleWindow):
     _iid_ = GUID("{00000117-0000-0000-C000-000000000046}")
 
-class IOleInPlaceActiveObjectPointer(COMPointer):
-    _interface_ = IOleInPlaceActiveObject
-
-
-IOleWindow._methods_ = [
+IOleWindow._methods_ = IUnknown._methods_ + [
     STDMETHOD(HRESULT, "GetWindow", POINTER(c_int)),
     STDMETHOD(HRESULT, "ContextSensitiveHelp", c_int)]
 
-IOleInPlaceUIWindow._methods_ = [
+IOleInPlaceUIWindow._methods_ = IOleWindow._methods_ + [
     STDMETHOD(HRESULT, "GetBorder", POINTER(RECT)),
     STDMETHOD(HRESULT, "RequestBorderSpace", POINTER(BORDERWIDTHS)),
     STDMETHOD(HRESULT, "SetBorderSpace", POINTER(BORDERWIDTHS)),
-    STDMETHOD(HRESULT, "SetActiveObject", IOleInPlaceActiveObjectPointer, POINTER(OLESTR))]
+    STDMETHOD(HRESULT, "SetActiveObject", POINTER(IOleInPlaceActiveObject), POINTER(OLESTR))]
 
-IOleInPlaceActiveObject._methods_ = [
+IOleInPlaceActiveObject._methods_ = IOleWindow._methods_ + [
     STDMETHOD(HRESULT, "TranslateAccelerator", POINTER(MSG)),
     STDMETHOD(HRESULT, "OnFrameWindowActivate", c_int),
     STDMETHOD(HRESULT, "OnDocWindowActivate", c_int),
-    STDMETHOD(HRESULT, "ResizeBorder", POINTER(RECT), IOleInPlaceUIWindowPointer, c_int),
+    STDMETHOD(HRESULT, "ResizeBorder", POINTER(RECT), POINTER(IOleInPlaceUIWindow), c_int),
     STDMETHOD(HRESULT, "EnableModeless", c_int)]
