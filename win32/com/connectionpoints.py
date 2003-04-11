82ab3152e861cd9cfb4186d7a3508618034c77ce
theller <none@none>
2003-04-11 19:49:09 +0000

    Expose a dispinterface_EventReceiver class. This is a base class helping to implement dispinterface notification sinks.
    --HG--
    branch : ctypes
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk/ctypes%4047671

--- win32/com/connectionpoints.py
+++ win32/com/connectionpoints.py
@@ -62,3 +62,43 @@ def GetConnectionPoint(comptr, event_interface):
     cp = pointer(IConnectionPoint())
     cpc.FindConnectionPoint(byref(event_interface._iid_), byref(cp))
     return cp
+
+################################################################
+# A Base class for events delivered to a dispinterface
+#
+from ctypes.com import COMObject
+
+class dispinterface_EventReceiver(COMObject):
+    # We fake the reference counts...
+    def AddRef(self, this):
+        return 2
+
+    def Release(self, this):
+        return 1
+
+    def _get_args(self, dp):
+        args = []
+        for i in range(dp.cArgs-1, -1, -1):
+            args.append(dp.rgvarg[i].get_value())
+        return tuple(args)
+
+
+    def Invoke(self,
+               this,      dispid,    refiid,    lcid,
+               wFlags, pDispParams, pVarResult, pExcepInfo,
+               puArgErr):
+        # It seems we don't have any alternative to implement Invoke
+        # in Python.  Although DispInvoke does work for dual
+        # interfaces, it doesn't work for dispinterfaces.  I have
+        # tried this and failed with DispInvoke, and I assume its the
+        # same with CreateStdDispatch.
+        mthname = self._com_interfaces_[0]._dispmap_[dispid]
+        args = self._get_args(pDispParams[0])
+        mth = getattr(self, mthname, None)
+        if mth is not None:
+            # For symmetry with other code, we *should* also pass the
+            # 'this' parameter, but we don't.
+            mth(*args)
+        else:
+            print "# Unimplemented (%s %s)" % (mthname, args)
+        return 0
