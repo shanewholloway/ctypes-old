9b6627a6e1c2d413eed0eb4174553f72c8f1c26a
theller <none@none>
2003-04-25 17:09:46 +0000

    Updated.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4047711

--- ctypes/docs/updateweb.cmd
+++ ctypes/docs/updateweb.cmd
@@ -1,4 +1,4 @@
 REM $Id$
 make_html -w
 scp ../LICENSE.txt default.css ctypes.html theller@starship.python.net:~/public_html/
-scp default.css faq.html tutorial.html reference.html theller@starship.python.net:~/public_html/ctypes/
+scp default.css changes.html faq.html tutorial.html reference.html theller@starship.python.net:~/public_html/ctypes/
