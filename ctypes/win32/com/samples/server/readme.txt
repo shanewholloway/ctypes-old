cb957a82ea76c86fe791721438ba5f811b3f63fb
theller <none@none>
2003-05-08 19:40:04 +0000

    A ctypes.com localserver sample.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4047748

---
+++ ctypes/win32/com/samples/server/readme.txt
@@ -0,0 +1,28 @@
+README for the ctypes.com samples
+
+sum.py is a COM localserver sample. It implements a dual interface
+described in a typelibrary. The source code is extensively commented,
+please refer to it.
+
+In short: 
+
+   sum.idl - the idl file describing the interface and the coclass.
+   sum.tlb - the idl file compiled into a type library (with MIDL)
+   sum_gen.py - the python wrapper created from the sum.tlb file.
+	        This must be recreated, because it contains the absolute pathname.
+   sum.py - the COM object implementation itself.
+
+sum.py accepts the '-regserver' and '-unregserver' command line flags.
+This will register or unregister the COM object and the type library.
+It also accepts the '/automation' command line flag, this is used to
+start the localserver, the object is registered with COM to receive
+this flag.
+
+sum.py can also be 'compiled' into an exe-file with py2exe, no special
+flags needed. Use the 'setup_sum.py' file to build the exe.
+
+The exe-file can be registered or unregistered with the same flags as
+above.
+
+Note: Currently py2exe cannot embed the type library into the
+exe-file, and the registration will raise an exception.
---
+++ ctypes/win32/com/samples/server/sum.py
@@ -0,0 +1,90 @@
+# A sample COM server, implementing a dual interface.
+#
+# The interface is described in sum.idl, which must be compiled into
+# the type libary sum.tlb with the MIDL compiler command 'midl /tlb
+# sum.tlb sum.idl'.
+#
+# Next, the type library should be used to create the Python wrapper
+# sum_gen.py. This can be done with the readtlb.py tool, with the
+# command '..\..\tools\readtlb.py sum.tlb >sum_gen.py'.
+
+# Then, we can import what we need from the sum_gen module:
+
+# interface, coclass, typelib
+from sum_gen import IDualSum, CSum, SumLib
+
+# This object implements a dual interface described in a type library,
+# and ctypes.com provides a handy bas class we can use:
+from ctypes.com.automation import DualObjImpl
+
+class SumObject(DualObjImpl):
+    _reg_clsctx_ = 0
+    # A sequence of COM interfaces this object implements
+    _com_interfaces_ = [IDualSum]
+    # The type library we need, SumLib has the correct attributes
+    _typelib_ = SumLib
+    # The progid for the registry
+    _reg_progid_ = "ctypes.SumObject"
+    # The name for the registry
+    _reg_desc_ = "Sum Object"
+    # and the clsid. We could use the one in the typelib, if the
+    # typelib contains a CoClass. The typelib could as well only
+    # describe an interface.
+##    _reg_clsid_ = '{2E0504A1-1A23-443F-939D-869A6C731521}'
+    _reg_clsid_ = CSum._reg_clsid_
+
+    # We *should* implement the methods described in the typelib.  COM
+    # servers receive an additional 'this' parameter, which is the COM
+    # 'this' pointer (an integer), although we don't need it.
+    #
+    # The name of the COM method must either be
+    # <interface_name>-<method_name> or simply <method_name>, but the
+    # former is preferred to avoid name clashes if the object
+    # implements a lot of interfaces.
+    def IDualSum_Add(self, this, a, b, presult):
+        # See the typelib, a and b are c_doubles converted to Python
+        # floats, and presult is the result pointer which also points
+        # to a c_double.
+        presult[0] = a + b
+        # The method must return a HRESULT, 0 is the same as S_OK
+        return 0
+    
+def main():
+    # Running this script with "-regserver" registers this object as a
+    # LocalServer32 together with the type library, "-unregserver"
+    # deletes the registry entries again.
+    #
+    # Note that the object will be registered to run with python.exe,
+    # *not* pythonw.exe, so a console will be created when the object
+    # is run. At least it is nice to see tracebacks if they occurr.
+    #
+    # The object will be registered to run with a "/automation"
+    # command line flag when started by COM.
+    #
+    # You can start the object from Oleview for example, look under
+    # Object Classes->All Objects->Sum Object to locate it.
+    #
+    # You can also (since it implements a dual interface) use it from win32com:
+    #
+    # from win32com.client import Dispatch
+    # d = Dispatch("ctypes.SumObject")
+    # print d.Add(3.14, 3.14)
+    #
+    # This script prints '6.28'.
+    #
+    import sys
+    from ctypes.com.register import register, unregister
+    if "-regserver" in sys.argv:
+        register(SumObject)
+        return
+    if "-unregserver" in sys.argv:
+        unregister(SumObject)
+        return
+    if "/automation" in sys.argv:
+        from ctypes.com.server import localserver
+        localserver(SumObject)
+        return
+    # Raise an error here?
+    
+if __name__ == '__main__':
+    main()
