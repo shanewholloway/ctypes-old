3965b416a3c83adf4e306cf924306b33fc2b2a96
theller <none@none>
2003-05-16 16:30:55 +0000

    Add a 'in_dll(name, dll)' class method to the types, this allows to access values exported from DLLs.
    Unittests for this feature.
    
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4047759

---
+++ ctypes/unittests/test_values.py
@@ -0,0 +1,66 @@
+"""
+A testcase which accesses *values* in a dll.
+"""
+
+import unittest
+from ctypes import *
+
+pydll = cdll.python22
+
+class ValuesTestCase(unittest.TestCase):
+    """A doc string"""
+
+    def test_optimizeflag(self):
+        """This test accesses the Py_OptimizeFlag intger, which is
+        exported by the Python dll.
+
+        It's value is set depending on the -O and -OO flags:
+        if not given, it is 0 and __debug__ is 1.
+        If -O is given, the flag is 1, for -OO it is 2.
+        docstrings are also removed in the latter case."""
+        opt = c_int.in_dll("Py_OptimizeFlag", pydll).value
+        print ValuesTestCase.__doc__
+        if __debug__:
+            self.failUnlessEqual(opt, 0)
+        elif ValuesTestCase.__doc__ is not None:
+            self.failUnlessEqual(opt, 1)
+        else:
+            self.failUnlessEqual(opt, 2)
+
+    def test_frozentable(self):
+        """Python exports a PyImport_FrozenModules symbol. This is a
+        pointer to an array of struct _frozen entries.  The end of the
+        array is marked by an entry containing a NULL name and zero
+        size.
+
+        In standard Python, this table contains a __hello__ module,
+        and a __phello__ package containing a spam module."""
+        class struct_frozen(Structure):
+            _fields_ = [("name", c_char_p),
+                        ("code", c_ubyte),
+                        ("size", c_int)]
+
+        FrozenTable = POINTER(struct_frozen)
+
+        ft = FrozenTable.in_dll("PyImport_FrozenModules", pydll)
+        # ft is a pointer to the struct_frozen entries:
+        items = []
+        for entry in ft:
+            # This is dangerous. We *can* iterate over a pointer, but
+            # the loop will not terminate (maybe with an access
+            # violation;-) because the pointer instance has no size.
+            if entry.name is None:
+                break
+            items.append((entry.name, entry.size))
+        expected = [("__hello__", 100), ("__phello__", -100), ("__phello__.spam", 100)]
+        self.failUnlessEqual(items, expected)
+
+def test(verbose=0):
+    runner = unittest.TextTestRunner(verbosity=verbose)
+    runner.run(get_suite())
+
+def get_suite():
+    return unittest.makeSuite(ValuesTestCase)
+
+if __name__ == '__main__':
+    unittest.main()
