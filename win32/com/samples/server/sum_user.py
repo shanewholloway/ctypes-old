8784f05be364ec9ae83b5ff250029f07ce5487e3
theller <none@none>
2003-05-22 18:48:38 +0000

    Demonstrate QueryInterface also, and GetTypeInfoCount().
    --HG--
    branch : ctypes
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk/ctypes%4047816

--- win32/com/samples/server/sum_user.py
+++ win32/com/samples/server/sum_user.py
@@ -1,10 +1,18 @@
 # ctypes client code, using the CSum COM object
-from sum_gen import CSum
+from ctypes import *
 from ctypes.com import CreateInstance
-from ctypes import c_double, byref
+from ctypes.com.automation import IDispatch
+from sum_gen import CSum
 
 sum = CreateInstance(CSum)
-
 result = c_double()
 sum.Add(3.14, 3.14, byref(result))
-print result
+print "Added 3.14 and 3.14, result", result.value
+
+idisp = pointer(IDispatch())
+
+sum.QueryInterface(byref(IDispatch._iid_), byref(idisp))
+
+count = c_uint()
+idisp.GetTypeInfoCount(byref(count))
+print "Has Typeinfo?", count.value
