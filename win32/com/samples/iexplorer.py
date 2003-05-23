80006afe26971b3abddfe6d287f2417960ed59d6
theller <none@none>
2003-05-23 11:17:27 +0000

    Clean up, add comments.
    --HG--
    branch : ctypes
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk/ctypes%4047818

--- win32/com/samples/iexplorer.py
+++ win32/com/samples/iexplorer.py
@@ -1,39 +1,30 @@
-from ctypes.com import IUnknown, GUID, REFCLSID, REFIID
-from ctypes.com.automation import BSTR, VARIANT
-from ctypes import *
-from ctypes.wintypes import *
+from ctypes import windll, byref, c_int
+from ctypes.wintypes import DWORD, MSG
 
-user32 = windll.user32
+from ctypes.com import CreateInstance
+from ctypes.com.automation import VARIANT
+from ctypes.com.connectionpoints import dispinterface_EventReceiver, GetConnectionPoint
 
 from ie6_gen import InternetExplorer, IWebBrowser2, DWebBrowserEvents2
 
 ################################################################
-# This should move into ctypes.com :
-from ctypes import oledll
-from ctypes.com import ole32
-
-ole32.CoInitialize(None)
-ole32.CoCreateInstance.argtypes = REFCLSID, c_voidp, DWORD, REFIID, POINTER(POINTER(IUnknown))
-
-CLSCTX_INPROC_SERVER = 0x1
-CLSCTX_LOCAL_SERVER = 0x4
-
-def CreateInstance(coclass, interface=None,
-                   clsctx = CLSCTX_LOCAL_SERVER):
-    if interface is None:
-        interface = coclass._com_interfaces_[0]
-    p = pointer(interface())
-    clsid = GUID(coclass._reg_clsid_)
-    ole32.CoCreateInstance(byref(clsid),
-                           0,
-                           clsctx,
-                           byref(interface._iid_),
-                           byref(p))
-    return p
-
-################################################################
-
-from ctypes.com.connectionpoints import dispinterface_EventReceiver
+#
+# dispinterface_EventReceiver is a base class which implements a
+# dispinterface. Subclasses must have the _com_interfaces_ attribute,
+# and the first item on this list must be a dispinterface class.  The
+# dispinterface metaclass builds a _dispmap_ attribute from the
+# _methods_ list, which is a dictionary mapping dispid integers to
+# method names.
+#
+# Finally the dispinterface_EventReceiver class has an Invoke method,
+# called by COM, which converts the argument array passed by COM to a
+# tuple of Python values, dynamically looks up the method to call, and
+# calls this method.  If no method is found, a warning is printed
+# containing the method name and the argument tuple.
+#
+# The only method we implement here is the OnQuit method which is
+# called when the Browser is closed, we use it to post q QuitMessage
+# to ourself so that the message loop terminates.
 
 class DWebBrowserEvents2Impl(dispinterface_EventReceiver):
     _com_interfaces_ = [DWebBrowserEvents2]
@@ -42,37 +33,51 @@ class DWebBrowserEvents2Impl(dispinterface_EventReceiver):
     # and simply print the method name with the arguments
     # for unimplemented methods.
     def OnQuit(self, this, *args):
-        print "OnQuit", self, this, args
-        user32.PostQuitMessage(0)
+        print "OnQuit", self, args
+        windll.user32.PostQuitMessage(0)
 
 ################################################################
-from ctypes.com.connectionpoints import GetConnectionPoint
 
+# Start InternetExplorer as ActiveX object
 browser = CreateInstance(InternetExplorer)
+# We can now call methods on it
 browser._put_Visible(1)
+v = c_int()
+browser._get_Visible(byref(v))
+print "Is visible?", hex(v.value)
 
+# But we also want to receive events from it via the
+# DWebBrowserEvents2 interface.
+
+# Get a connection point, this is a pointer to a IConnectionPoint
+# interface.
 cp = GetConnectionPoint(browser, DWebBrowserEvents2)
 
+# We have to implement the COM object which will receive the events,
+# and retrieve a pointer to the DWebBrowserEvents2 interface:
 comobj = DWebBrowserEvents2Impl()
 # _com_pointers_ is a sequence of (guid, interface_pointer) pairs.
 # Get the interface_pointer to the first (default) interface.
 pevents = comobj._com_pointers_[0][1]
 
+# We call the Advise method on the IConnectionPoint interface, we supply
+# a pointer to a DWORD where a value will be stored which is used to identify
+# the connection:
 cookie = DWORD()
 cp.Advise(byref(pevents), byref(cookie))
+# The connection is now established.
 
+# Call the Navigate method. We have to supply empty values for all arguments:
 v = VARIANT()
 browser.Navigate("http://www.python.org/",
                  byref(v), byref(v), byref(v), byref(v))
 
-v = c_int()
-browser._get_Visible(byref(v))
-print "Is visible?", hex(v.value)
+# To receive events, we have to run a message loop:
+def pump_messages():
+    user32 = windll.user32
+    msg = MSG()
+    while user32.GetMessageA(byref(msg), None, 0, 0):
+        user32.TranslateMessage(byref(msg))
+        user32.DispatchMessageA(byref(msg))
 
-from ctypes.wintypes import MSG
-msg = MSG()
-while user32.GetMessageA(byref(msg), None, 0, 0):
-    user32.TranslateMessage(byref(msg))
-    user32.DispatchMessageA(byref(msg))
-
-ole32.CoUninitialize()
+pump_messages()
