0830bc6a3e423361d64155c8826a36500b0b0a16
theller <none@none>
2003-05-23 19:45:04 +0000

    Towards inproc server...
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4047826

--- ctypes/win32/com/server.py
+++ ctypes/win32/com/server.py
@@ -1,9 +1,10 @@
 from ctypes.com import IUnknown, PIUnknown, COMObject, ole32, \
      GUID, HRESULT, STDMETHOD, REFIID
 from ctypes.com.automation import IDispatch, oleaut32
-from ctypes import c_int, c_ulong, byref, c_voidp, Structure, windll, POINTER, pointer
+from ctypes import *
 
 user32 = windll.user32
+kernel32 = windll.kernel32
 ole32.CoInitialize(None)
 
 # We need to call this when we're done:
@@ -46,6 +47,123 @@ class IExternalConnection(IUnknown):
         STDMETHOD(HRESULT, "ReleaseConnection", DWORD, DWORD, DWORD)]
 
 ################################################################
+
+# Hm. We cannot redirect sys.stderr/sys.stdout in the inproc case,
+# If the process is Python, the user would be pissed off if we did.
+class _Logger(object):
+    # Redirect standard output and standard error to
+    # win32 Debug Messages. Output can be viewed for example
+    # in DebugView from www.sysinternals.com
+    _installed = 0
+    _text = ""
+    def write(self, text):
+        self._text += str(text)
+        if "\n" in self._text:
+            kernel32.OutputDebugStringA(self._text)
+            self._text = ""
+
+    def install(cls):
+        if cls._installed:
+            return
+        import sys
+        sys.stdout = sys.stderr = cls()
+        cls._installed = 1
+    install = classmethod(install)
+
+    def isatty(self):
+        return 0
+
+def inproc_find_class(clsid):
+    import _winreg
+##    print str(clsid)
+    key = _winreg.OpenKey(_winreg.HKEY_CLASSES_ROOT, "CLSID\\%s" % clsid)
+    import sys
+    try:
+        pathdir = _winreg.QueryValue(key, "PythonPath")
+    except WindowsError:
+        pass
+    else:
+        if not pathdir in sys.path:
+            sys.path.insert(0, pathdir)
+##            print "appended %s to sys.path" % pathdir
+##            print "SYS.PATH", sys.path
+    pythonclass = _winreg.QueryValue(key, "PythonClass")
+##    print pythonclass
+    parts = pythonclass.split(".")
+    modname = ".".join(parts[:-1])
+    classname = parts[-1]
+    __import__(modname)
+    mod = sys.modules[modname]
+##    print "imported", mod
+
+    # It was a nice idea to 'reload' the module, so that during
+    # debugging we would always run uptodate versions of the code.
+    # The problem is that super(type, obj) sometimes doesn't work
+    # correctly anymore because 'obj' would not be an instance of
+    # 'type' anymore.
+    #
+    # So, unfortuately, we cannot use this.
+##    if __debug__:
+##        reload(mod)
+
+##    print "returning", getattr(mod, classname)
+    return getattr(mod, classname)
+
+# Fake implementation, with hardcoded names
+def inproc_find_class(clsid):
+    import sys
+    pathdir = r"c:\sf\ctypes_head\win32\com\samples\server"
+    if not pathdir in sys.path:
+        sys.path.insert(0, pathdir)
+    classname = "SumObject"
+    modname = "sum"
+    __import__(modname)
+    mod = sys.modules[modname]
+    return getattr(mod, classname)
+
+def DllGetClassObject(rclsid, riid, ppv):
+    # This function is called by C code, and receives C integers as
+    # parameters. rcslid is a pointer to the CLSID for the coclass we
+    # want to be created, riid is a pointer to the requested
+    # interface.
+    _Logger.install()
+
+    iid = GUID.from_address(riid)
+    clsid = GUID.from_address(rclsid)
+    p = PIUnknown.from_address(ppv)
+
+    print "\tDllGetClassObject called with", clsid, iid
+    # Use the clsid to find additional info in the registry.
+    cls = inproc_find_class(clsid)
+    # XXX Hm, does inproc_findclass return None, or raise an Exception?
+    if not cls:
+        return CLASS_E_CLASSNOTAVAILABLE
+    global _active_objects
+    factory = ClassFactory(cls)
+    _active_objects.append(factory)
+    obj = pointer(factory._com_pointers_[0][1])
+    # obj is a pointer to the class factory's IClassFactory interface.
+    return obj.QueryInterface(byref(iid), byref(p))
+
+_active_objects = []
+
+S_FALSE = 0x00000001
+S_OK = 0x00000000
+
+def DllCanUnloadNow():
+    # XXX TODO: Read about inproc server refcounting in Don Box
+    return S_FALSE
+    _Logger.install()
+    if _active_objects:
+        print "* DllCanUnloadNow -> S_FALSE", _active_objects
+        return S_FALSE
+    else:
+        print "* DllCanUnloadNow -> S_OK"
+        return S_OK
+    # Hm Call ole32.CoUnitialize here?
+
+
+################################################################
 #
 # Algorithm for the server lifetime taken from Don Box: Essential COM
 # (German edition, chapter 6.3: Lebensdauer von Server-Prozessen)
@@ -60,9 +178,12 @@ class ClassFactory(COMObject):
         self.objclass = objclass
 
     def get_interface_pointer(self, interface=IUnknown):
+        # XXX Should this be reworked to return itf instead of byref(itf)?
+        # Or should it return pointer(itf)?
         for iid, itf in self._com_pointers_:
             if interface._iid_ == iid:
                 return byref(itf)
+        # and shouldn't we raise an exception here?
 
     def _register_class(self, regcls=REGCLS_MULTIPLEUSE):
         cookie = c_ulong()
@@ -80,9 +201,11 @@ class ClassFactory(COMObject):
     # IUnknown methods
 
     def AddRef(self, this):
+        print "AddRef", self
         return 2
 
     def Release(self, this):
+        print "Release", self
         return 1
 
     # IClassFactory methods
