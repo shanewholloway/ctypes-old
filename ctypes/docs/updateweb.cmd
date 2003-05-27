27e39e6b015fd1979105d6e538fc2aff6f66e75f
theller <none@none>
2003-05-27 17:30:42 +0000

    Fix links, other updates.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4047831

--- ctypes/docs/updateweb.cmd
+++ ctypes/docs/updateweb.cmd
@@ -1,4 +1,3 @@
 REM $Id$
 make_html -w
-scp ../LICENSE.txt default.css ctypes.html theller@starship.python.net:~/public_html/
-scp default.css changes.html com.html faq.html index.html sum_sample.html tutorial.html reference.html theller@starship.python.net:~/public_html/ctypes/
+scp ../LICENSE.txt default.css changes.html com.html faq.html index.html sum_sample.html tutorial.html reference.html theller@starship.python.net:~/public_html/ctypes/
