403bc7ec693eb1032f99ad6d8c35ceaf03c8de1e
theller <none@none>
2003-05-28 19:53:43 +0000

    Allow to select inproc_server or local_server on the command line.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4047835

--- ctypes/win32/com/samples/server/sum_user.py
+++ ctypes/win32/com/samples/server/sum_user.py
@@ -1,10 +1,23 @@
 # ctypes client code, using the CSum COM object
+##import pywintypes
+
 from ctypes import *
 from ctypes.com import CreateInstance
 from ctypes.com.automation import IDispatch
 from sum_gen import CSum
 
-sum = CreateInstance(CSum)
+CLSCTX_INPROC_SERVER = 0x1
+CLSCTX_LOCAL_SERVER = 0x4
+
+import sys
+if "inproc_server" in sys.argv:
+    print "Using CLSCTX_INPROC_SERVER"
+    clsctx = CLSCTX_INPROC_SERVER
+else:
+    print "Using CLSCTX_LOCAL_SERVER"
+    clsctx = CLSCTX_LOCAL_SERVER
+
+sum = CreateInstance(CSum, clsctx=clsctx)
 result = c_double()
 sum.Add(3.14, 3.14, byref(result))
 print "Added 3.14 and 3.14, result", result.value
@@ -16,3 +29,10 @@ sum.QueryInterface(byref(IDispatch._iid_), byref(idisp))
 count = c_uint()
 idisp.GetTypeInfoCount(byref(count))
 print "Has Typeinfo?", count.value
+
+del idisp
+print "After del idisp"
+import gc
+gc.collect()
+
+print "After gc.collect()"
