347d8ac78bac93e130814979d405949e90abb6bb
theller <none@none>
2003-06-12 19:39:35 +0000

    *** empty log message ***
    --HG--
    branch : ctypes
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk/ctypes%4047854

--- win32/com/samples/server/test_w32.py
+++ win32/com/samples/server/test_w32.py
@@ -1,5 +1,18 @@
 # win32com client code to use the CSum COM object
 
+import sys
+
+CLSCTX_INPROC_SERVER = 0x1
+CLSCTX_LOCAL_SERVER = 0x4
+
+import sys
+if "inproc_server" in sys.argv:
+    print "Using CLSCTX_INPROC_SERVER"
+    clsctx = CLSCTX_INPROC_SERVER
+else:
+    print "Using CLSCTX_LOCAL_SERVER"
+    clsctx = CLSCTX_LOCAL_SERVER
+
 from win32com.client import Dispatch
-d = Dispatch("ctypes.SumObject")
-print d.Add(3.14, 3.14)
+d = Dispatch("ctypes.SumObject", clsctx=clsctx)
+assert 6.28 == d.Add(3.14, 3.14)
