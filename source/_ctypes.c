cf841e560872566f5712727c2a1d71e2120a3988
theller <none@none>
2003-06-20 12:11:43 +0000

    new test function for callbacks returning doubles
    --HG--
    branch : ctypes
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk/ctypes%4047858

--- source/_ctypes.c
+++ source/_ctypes.c
@@ -3457,6 +3457,20 @@ EXPORT int _testfunc_byval(point in, point *pout)
 
 #endif
 
+EXPORT double 
+integrate(double a, double b, double (*f)(double), long nstep)
+{
+	double x, sum=0.0, dx=(b-a)/(double)nstep;
+	printf("called with %f, %f, %p, %ld\n", a, b, f, nstep);
+	for(x=a+0.5*dx; (b-x)*(x-a)>0.0; x+=dx){
+		double result;
+		result = f(x);
+		printf("f(%f) -> %f\n", x, result);
+		sum += result;
+	}
+	return sum/(double)nstep;
+}
+
 /*
  Local Variables:
  compile-command: "cd .. && python setup.py -q build -g && python setup.py -q build install --home ~"
