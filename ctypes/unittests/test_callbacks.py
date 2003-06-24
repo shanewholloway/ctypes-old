2fc233b6f3085a8e0ac4c9e8f655fafcdc826809
theller <none@none>
2003-06-24 19:29:03 +0000

    Callback functions can now return data types different from integer. Windows only, currently.
    Test this.
    
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4047867

---
+++ ctypes/unittests/test_callbacks.py
@@ -0,0 +1,36 @@
+import unittest
+from ctypes import *
+
+class CallbacksTestCase(unittest.TestCase):
+
+    def test_integrate(self):
+        # Derived from some then non-working code, posted by David Foster
+        import _ctypes
+
+        # The function prototype called by 'integrate': double func(double);
+        CALLBACK = CFUNCTYPE(c_double, c_double)
+
+        # The integrate function itself, exposed from the _ctypes dll
+        integrate = CDLL(_ctypes.__file__).integrate
+        integrate.argtypes = (c_double, c_double, CALLBACK, c_long)
+        integrate.restype = c_double
+
+        def func(x):
+            return x**2
+
+        result = integrate(0.0, 1.0, CALLBACK(func), 10)
+        diff = abs(result - 1./3.)
+        
+        self.failUnless(diff < 0.01)
+                                   
+        
+
+def get_suite():
+    return unittest.makeSuite(CallbacksTestCase)
+
+def test(verbose=0):
+    runner = unittest.TextTestRunner(verbosity=verbose)
+    runner.run(get_suite())
+
+if __name__ == '__main__':
+    unittest.main()
