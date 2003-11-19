f32534d5b4b97e6b4568005611bdf3ab43b7f212
theller <none@none>
2003-11-19 20:58:37 +0000

    I'm now using Python 2.3 per default.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4047964

--- ctypes/win32/com/samples/server/rt.cmd
+++ ctypes/win32/com/samples/server/rt.cmd
@@ -1,7 +1,7 @@
 @echo off
 @setlocal
-@set _exe=c:\python22\python.exe
-@if "%1"=="-d" set _exe=c:\python22\PCBuild\python_d.exe
+@set _exe=c:\python23\python.exe
+@if "%1"=="-d" set _exe=c:\sf\python\dist\src-maint23\PCBuild\python_d.exe
 
 echo "Registering ctypes server"
 %_exe% sum.py -regserver
@@ -11,8 +11,8 @@ echo "Un-Registering ctypes server"
 %_exe% sum.py -unregserver
 
 echo "Registering win32com server"
-%_exe% w32server.py --regserver
+%_exe% w32server.py --register
 %_exe% w32_user.py local_server
 %_exe% w32_user.py inproc_server
 echo "Un-Registering win32com server"
-%_exe% w32server.py --unregserver
+%_exe% w32server.py --unregister
