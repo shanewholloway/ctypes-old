afe7d0b7d0fca6ccb70602fbc4e532255d3fcedf
theller <none@none>
2003-12-05 16:03:10 +0000

    Add a test which creates a CFuncPtr from an integer address.
    --HG--
    branch : ctypes
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk/ctypes%4047981

--- unittests/test_returnfuncptrs.py
+++ unittests/test_returnfuncptrs.py
@@ -4,10 +4,9 @@ import _ctypes
 
 class ReturnFuncPtrTestCase(unittest.TestCase):
 
-    def test(self):
-        import _ctypes
+    def test_with_prototype(self):
         dll = CDLL(_ctypes.__file__)
-        # The _ctypes shard lib/dll exports quite some functions for testing.
+        # The _ctypes shared lib/dll exports quite some functions for testing.
         # The get_strchr function returns a *pointer* to the C strchr function.
         get_strchr = dll.get_strchr
         get_strchr.restype = CFUNCTYPE(c_char_p, c_char_p, c_char)
@@ -17,6 +16,18 @@ class ReturnFuncPtrTestCase(unittest.TestCase):
         self.assertRaises(TypeError, strchr, "abcdef", 3)
         self.assertRaises(TypeError, strchr, "abcdef")
         
+    def test__without_prototype(self):
+        dll = CDLL(_ctypes.__file__)
+        get_strchr = dll.get_strchr
+        addr = get_strchr()
+        # _CFuncPtr instances are now callable with an integer argument
+        # which denotes a function address:
+        strchr = CFUNCTYPE(c_char_p, c_char_p, c_char)(addr)
+        self.failUnless(strchr("abcdef", "b"), "bcdef")
+        self.failUnlessEqual(strchr("abcdef", "x"), None)
+        self.assertRaises(TypeError, strchr, "abcdef", 3)
+        self.assertRaises(TypeError, strchr, "abcdef")
+
 def get_suite():
     return unittest.makeSuite(ReturnFuncPtrTestCase)
 
