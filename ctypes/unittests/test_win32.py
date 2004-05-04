c7dbdad73eab3b65318f6149105f7c82d5d506f9
theller <none@none>
2004-05-04 09:16:23 +0000

    Some test code has to be reorganized.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4048163

--- ctypes/unittests/test_win32.py
+++ ctypes/unittests/test_win32.py
@@ -1,54 +1,45 @@
 # Windows specific tests
 
 from ctypes import *
-import unittest
+import unittest, sys
 
-class WindowsTestCase(unittest.TestCase):
-    def test_callconv_1(self):
-        "Call functions with the wrong number of arguments, or the wrong calling convention"
-        GetModuleHandle = windll.kernel32.GetModuleHandleA
+if sys.platform == "win32":
 
-        # ValueError: Procedure probably called with not enough arguments (4 bytes missing)
-        self.assertRaises(ValueError, GetModuleHandle)
+    class WindowsTestCase(unittest.TestCase):
+        def test_callconv_1(self):
+            "Call functions with the wrong number of arguments, or the wrong calling convention"
+            GetModuleHandle = windll.kernel32.GetModuleHandleA
 
-        # This one should succeeed...
-        self.failUnless(GetModuleHandle(None))
+            # ValueError: Procedure probably called with not enough arguments (4 bytes missing)
+            self.assertRaises(ValueError, GetModuleHandle)
 
-        # ValueError: Procedure probably called with too many arguments (8 bytes in excess)
-        self.assertRaises(ValueError, GetModuleHandle, 0, 0, 0)
+            # This one should succeeed...
+            self.failUnless(GetModuleHandle(None))
 
-    def test_callconv_2(self):
-        "Call functions with the wrong number of arguments, or the wrong calling convention"
-        GetModuleHandleA = cdll.kernel32.GetModuleHandleA
+            # ValueError: Procedure probably called with too many arguments (8 bytes in excess)
+            self.assertRaises(ValueError, GetModuleHandle, 0, 0, 0)
 
-        # ValueError: Procedure called with not enough arguments (4 bytes missing)
-        # or wrong calling convention
-        self.assertRaises(ValueError, GetModuleHandleA, None)
-        self.assertRaises(ValueError, GetModuleHandleA)
+        def test_callconv_2(self):
+            "Call functions with the wrong number of arguments, or the wrong calling convention"
+            GetModuleHandleA = cdll.kernel32.GetModuleHandleA
 
-    def test_SEH(self):
-        """Call functions with invalid arguments, and make sure that access violations
-        are trapped and raise an exception"""
-        self.assertRaises(WindowsError, windll.kernel32.GetModuleHandleA, 32)
+            # ValueError: Procedure called with not enough arguments (4 bytes missing)
+            # or wrong calling convention
+            self.assertRaises(ValueError, GetModuleHandleA, None)
+            self.assertRaises(ValueError, GetModuleHandleA)
 
-    # TODO: Add tests for passing structures (and unions?) by value.
-    def test_struct_by_value(self):
-        from ctypes.wintypes import POINT, RECT
+        def test_SEH(self):
+            """Call functions with invalid arguments, and make sure that access violations
+            are trapped and raise an exception"""
+            self.assertRaises(WindowsError, windll.kernel32.GetModuleHandleA, 32)
 
-        pt = POINT(10, 10)
-        rect = RECT(0, 0, 20, 20)
-        self.failUnlessEqual(True, windll.user32.PtInRect(byref(rect), pt))
+        # TODO: Add tests for passing structures (and unions?) by value.
+        def test_struct_by_value(self):
+            from ctypes.wintypes import POINT, RECT
 
-def get_suite():
-    import os
-    if os.name == "nt":
-        return unittest.makeSuite(WindowsTestCase)
-    else:
-        return None
-
-def test(verbose=0):
-    runner = unittest.TextTestRunner(verbosity=verbose)
-    runner.run(get_suite())
+            pt = POINT(10, 10)
+            rect = RECT(0, 0, 20, 20)
+            self.failUnlessEqual(True, windll.user32.PtInRect(byref(rect), pt))
 
 if __name__ == '__main__':
     unittest.main()
