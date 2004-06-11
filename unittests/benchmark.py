02781183401fdec843b237a46d00f0c0cfb712bc
theller <none@none>
2004-06-11 11:25:11 +0000

    Add comments with the preformance results on my machine, win32 and suse.
    --HG--
    branch : ctypes
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk/ctypes%4048259

--- unittests/benchmark.py
+++ unittests/benchmark.py
@@ -12,13 +12,21 @@ if __name__ == "__main__":
     from timeit import Timer
 
     t = Timer("ex_func_si('abc', 3)", "from __main__ import ex_func_si")
+    # win32: 1.284 us
+    # suse9.1 (vmware): 2.314 us
     print >> sys.stderr, "dll call, 2 args:  %.3f us" % t.timeit()
     t = Timer("py_func_si('abc', 3)", "from __main__ import py_func_si")
+    # win32: 0.629 us
+    # suse9.1(vmware): 0.910 us
     print >> sys.stderr, "func call, 2 args: %.3f us" % t.timeit()
     print >> sys.stderr
 
     t = Timer("ex_func()", "from __main__ import ex_func")
+    # win32: 0.773 us
+    # suse9.1(vmware): 1.521 us
     print >> sys.stderr, "dll call, no args:  %.3f us" % (t.timeit(10000000) / 10)
     t = Timer("py_func()", "from __main__ import py_func")
+    # win32: 0.139 us
+    # suse9.1(vmware): 0.231 us
     print >> sys.stderr, "func call, no args: %.3f us" % (t.timeit(10000000) / 10)
     print >> sys.stderr
