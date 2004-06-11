0643a4ec0be339f32794fecb9009c0a65a64aa5b
theller <none@none>
2004-06-11 09:08:55 +0000

    More argtypes specs (for solaris).
    --HG--
    branch : ctypes
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk/ctypes%4048256

--- unittests/test_cfuncs.py
+++ unittests/test_cfuncs.py
@@ -9,14 +9,18 @@ class CFunctions(unittest.TestCase):
 
     def test_byte(self):
         self.dll.tf_b.restype = c_byte
+        self.dll.tf_b.argtypes = (c_byte,)
         self.failUnlessEqual(self.dll.tf_b(-42), -42)
         self.dll.tf_bb.restype = c_byte
+        self.dll.tf_bb.argtypes = (c_byte, c_byte)
         self.failUnlessEqual(self.dll.tf_bb(0, -42), -42)
 
     def test_ubyte(self):
         self.dll.tf_B.restype = c_ubyte
+        self.dll.tf_B.argtypes = (c_ubyte,)
         self.failUnlessEqual(self.dll.tf_B(42), 42)
         self.dll.tf_bB.restype = c_ubyte
+        self.dll.tf_bB.argtypes = (c_byte, c_ubyte)
         self.failUnlessEqual(self.dll.tf_bB(0, 42), 42)
 
     def test_short(self):
