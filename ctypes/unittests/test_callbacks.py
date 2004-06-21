cfe989c62bc0a5bf62c0251dfd6ee4bbf1d98e1c
theller <none@none>
2004-06-21 11:31:00 +0000

    Refactored.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4048294

--- ctypes/unittests/test_callbacks.py
+++ ctypes/unittests/test_callbacks.py
@@ -10,11 +10,20 @@ class Callbacks(unittest.TestCase):
 
     def check_type(self, typ, arg):
         PROTO = self.functype.im_func(typ, typ)
-        return PROTO(self.callback)(arg)
+        result = PROTO(self.callback)(arg)
+        if typ == c_float:
+            self.failUnlessAlmostEqual(result, arg, places=5)
+        else:
+            self.failUnlessEqual(self.got_args, (arg,))
+            self.failUnlessEqual(result, arg)
 
-    def check_type_1(self, typ, arg):
         PROTO = self.functype.im_func(typ, c_byte, typ)
-        return PROTO(self.callback)(-3, arg)
+        result = PROTO(self.callback)(-3, arg)
+        if typ == c_float:
+            self.failUnlessAlmostEqual(result, arg, places=5)
+        else:
+            self.failUnlessEqual(self.got_args, (-3, arg))
+            self.failUnlessEqual(result, arg)
 
     if not hasattr(unittest.TestCase, "failUnlessAlmostEqual"):
         # This method is not present in Python 2.2
@@ -34,92 +43,57 @@ class Callbacks(unittest.TestCase):
     ################
 
     def test_byte(self):
-        self.failUnlessEqual(self.check_type(c_byte, 42), 42)
-        self.failUnlessEqual(self.got_args, (42,))
-        self.failUnlessEqual(self.check_type_1(c_byte, 42), 42)
-        self.failUnlessEqual(self.got_args, (-3, 42))
+        self.check_type(c_byte, 42)
+        self.check_type(c_byte, -42)
 
     def test_ubyte(self):
-        self.failUnlessEqual(self.check_type(c_ubyte, 42), 42)
-        self.failUnlessEqual(self.got_args, (42,))
-        self.failUnlessEqual(self.check_type_1(c_ubyte, 42), 42)
-        self.failUnlessEqual(self.got_args, (-3, 42))
+        self.check_type(c_ubyte, 42)
 
     def test_short(self):
-        self.failUnlessEqual(self.check_type(c_short, 42), 42)
-        self.failUnlessEqual(self.got_args, (42,))
-        self.failUnlessEqual(self.check_type_1(c_short, 42), 42)
-        self.failUnlessEqual(self.got_args, (-3, 42))
+        self.check_type(c_short, 42)
+        self.check_type(c_short, -42)
 
     def test_ushort(self):
-        self.failUnlessEqual(self.check_type(c_ushort, 42), 42)
-        self.failUnlessEqual(self.got_args, (42,))
-        self.failUnlessEqual(self.check_type_1(c_ushort, 42), 42)
-        self.failUnlessEqual(self.got_args, (-3, 42))
+        self.check_type(c_ushort, 42)
 
     def test_int(self):
-        self.failUnlessEqual(self.check_type(c_int, 42), 42)
-        self.failUnlessEqual(self.got_args, (42,))
-        self.failUnlessEqual(self.check_type_1(c_int, 42), 42)
-        self.failUnlessEqual(self.got_args, (-3, 42))
+        self.check_type(c_int, 42)
+        self.check_type(c_int, -42)
 
     def test_uint(self):
-        self.failUnlessEqual(self.check_type(c_uint, 42), 42)
-        self.failUnlessEqual(self.got_args, (42,))
-        self.failUnlessEqual(self.check_type_1(c_uint, 42), 42)
-        self.failUnlessEqual(self.got_args, (-3, 42))
+        self.check_type(c_uint, 42)
 
     def test_long(self):
-        self.failUnlessEqual(self.check_type(c_long, 42), 42)
-        self.failUnlessEqual(self.got_args, (42,))
-        self.failUnlessEqual(self.check_type_1(c_long, 42), 42)
-        self.failUnlessEqual(self.got_args, (-3, 42))
+        self.check_type(c_long, 42)
+        self.check_type(c_long, -42)
 
     def test_ulong(self):
-        self.failUnlessEqual(self.check_type(c_ulong, 42), 42)
-        self.failUnlessEqual(self.got_args, (42,))
-        self.failUnlessEqual(self.check_type_1(c_ulong, 42), 42)
-        self.failUnlessEqual(self.got_args, (-3, 42))
+        self.check_type(c_ulong, 42)
 
     def test_longlong(self):
-        self.failUnlessEqual(self.check_type(c_longlong, 42), 42)
-        self.failUnlessEqual(self.got_args, (42,))
-        self.failUnlessEqual(self.check_type_1(c_longlong, 42), 42)
-        self.failUnlessEqual(self.got_args, (-3, 42))
+        self.check_type(c_longlong, 42)
+        self.check_type(c_longlong, -42)
 
     def test_ulonglong(self):
-        self.failUnlessEqual(self.check_type(c_ulonglong, 42), 42)
-        self.failUnlessEqual(self.got_args, (42,))
-        self.failUnlessEqual(self.check_type_1(c_ulonglong, 42), 42)
-        self.failUnlessEqual(self.got_args, (-3, 42))
+        self.check_type(c_ulonglong, 42)
 
     def test_float(self):
         # only almost equal: double -> float -> double
         import math
-        self.failUnlessAlmostEqual(self.check_type(c_float, math.e), math.e,
-                                   places=6)
-        self.failUnlessAlmostEqual(self.got_args[0], math.e, places=6)
-        self.failUnlessAlmostEqual(self.check_type_1(c_float, math.e), math.e,
-                                   places=6)
-        self.failUnlessAlmostEqual(self.got_args[1], math.e, places=6)
+        self.check_type(c_float, math.e)
+        self.check_type(c_float, -math.e)
 
     def test_double(self):
-        self.failUnlessEqual(self.check_type(c_double, 3.14), 3.14)
-        self.failUnlessEqual(self.got_args, (3.14,))
-        self.failUnlessEqual(self.check_type_1(c_double, 3.14), 3.14)
-        self.failUnlessEqual(self.got_args, (-3, 3.14))
+        self.check_type(c_double, 3.14)
+        self.check_type(c_double, -3.14)
 
     def test_char(self):
-        self.failUnlessEqual(self.check_type(c_char, "x"), "x")
-        self.failUnlessEqual(self.got_args, ("x",))
-        self.failUnlessEqual(self.check_type_1(c_char, "x"), "x")
-        self.failUnlessEqual(self.got_args, (-3, "x"))
+        self.check_type(c_char, "x")
+        self.check_type(c_char, "a")
 
     def test_char_p(self):
-        self.failUnlessEqual(self.check_type(c_char_p, "abc"), "abc")
-        self.failUnlessEqual(self.got_args, ("abc",))
-        self.failUnlessEqual(self.check_type_1(c_char_p, "abc"), "abc")
-        self.failUnlessEqual(self.got_args, (-3, "abc"))
+        self.check_type(c_char_p, "abc")
+        self.check_type(c_char_p, "def")
 
 try:
     WINFUNCTYPE
