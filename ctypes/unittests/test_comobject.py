68ff7a781ae099096e0466af423ec29e85740cae
theller <none@none>
2004-07-20 15:17:32 +0000

    Add a test case which tests the automatic AddRef().
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4048358

--- ctypes/unittests/test_comobject.py
+++ ctypes/unittests/test_comobject.py
@@ -11,24 +11,49 @@ if sys.platform == "win32":
     class ComTestCase(unittest.TestCase):
         def __init__(self, *args):
             unittest.TestCase.__init__(self, *args)
-            global COMObject, IUnknown, GUID
-            from ctypes.com import COMObject, IUnknown, GUID
+            global COMObject, IUnknown, GUID, STDMETHOD, HRESULT
+            from ctypes.com import COMObject, IUnknown, GUID, STDMETHOD, HRESULT
 
         def setUp(self):
+            class IObjectWithSite(IUnknown):
+                _iid_ = GUID("{FC4801A3-2BA9-11CF-A229-00AA003D7352}")
+                _methods_ = IUnknown._methods_ + [ 
+                    (STDMETHOD(HRESULT, "SetSite", POINTER(IUnknown))),
+                    (STDMETHOD(HRESULT, "GetSite", POINTER(GUID), POINTER(c_void_p)))]
+
             class Factory(object):
                 def LockServer(self, *args):
                     pass
 
             class MyObject(COMObject):
                 _factory = Factory()
-                _com_interfaces_ = [IUnknown]
+                _com_interfaces_ = [IObjectWithSite]
+
+                def IObjectWithSite_SetSite(self, this, pUnkSite):
+                    self.m_site = pUnkSite
+                    return 0 # S_OK
 
             self.impl = MyObject()
 
         def tearDown(self):
             self.impl = None
+            import gc
+            gc.collect()
 
         ################
+
+        def test_site(self):
+            impl = self.impl
+            p = pointer(impl._com_pointers_[0][1])
+            self.failUnlessEqual(impl._refcnt, 0)
+            p.AddRef()
+            self.failUnlessEqual(impl._refcnt, 1)
+            p.SetSite(p)
+            self.failUnlessEqual(impl._refcnt, 2)
+            p.SetSite(None)
+            self.failUnlessEqual(impl._refcnt, 1)
+            del p
+            self.failUnlessEqual(impl._refcnt, 0)
 
         def test_comobject(self):
             impl = self.impl
@@ -51,6 +76,8 @@ if sys.platform == "win32":
             p2 = POINTER(IUnknown)()
             p.QueryInterface(byref(IUnknown._iid_), byref(p2))
             self.failUnlessEqual(get_refcnt(p), 2)
+            del p2
+            self.failUnlessEqual(get_refcnt(p), 1)
 
         def test_from_progid(self):
             g = GUID.from_progid("InternetExplorer.Application")
