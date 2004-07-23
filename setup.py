63803aa66cb0941c4a9ecb8dd6dbaacdeb3cc931
theller <none@none>
2004-07-23 14:52:25 +0000

    Require Python 2.3 (and check this early).
    --HG--
    branch : ctypes
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk/ctypes%4048415

--- setup.py
+++ setup.py
@@ -13,9 +13,14 @@ from __future__ import generators
 
 LIBFFI_SOURCES='source/gcc/libffi'
 
+__version__ = "0.9.0"
+
 ################################################################
 
 import os, sys
+
+if sys.version_info < (2, 3):
+    raise Exception, "ctypes %s requires Python 2.3 or better" % __version__
 
 from distutils.core import setup, Extension, Command
 import distutils.core
@@ -316,42 +321,10 @@ class my_build_py(build_py.build_py):
 
 ################################################################
 
-try:
-    walk = os.walk
-except AttributeError:
-    def walk(top):
-        # Python 2.3's os.walk generator, without the large docstring ;-)
-        # And without the topdown and onerror arguments.
-        from os.path import join, isdir, islink
-
-        # We may not have read permission for top, in which case we can't
-        # get a list of the files the directory contains.  os.path.walk
-        # always suppressed the exception then, rather than blow up for a
-        # minor reason when (say) a thousand readable directories are still
-        # left to visit.  That logic is copied here.
-        try:
-            names = os.listdir(top)
-        except OSError, err:
-            return
-
-        dirs, nondirs = [], []
-        for name in names:
-            if isdir(join(top, name)):
-                dirs.append(name)
-            else:
-                nondirs.append(name)
-
-        yield top, dirs, nondirs
-        for name in dirs:
-            path = join(top, name)
-            if not islink(path):
-                for x in walk(path):
-                    yield x
-
 def find_file_in_subdir(dirname, filename):
     # if <filename> is in <dirname> or any subdirectory thereof,
     # return the directory name, else None
-    for d, _, names in walk(dirname):
+    for d, _, names in os.walk(dirname):
         if filename in names:
             return d
     return None
@@ -450,7 +423,7 @@ if __name__ == '__main__':
           packages = packages,
           data_files = data_files,
 
-          version="0.9.0",
+          version=__version__,
           description="create and manipulate C data types in Python",
           long_description = __doc__,
           author="Thomas Heller",
