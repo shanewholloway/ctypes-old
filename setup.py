2198a4b0fded29fa76efe451ef03692320aeaa0c
theller <none@none>
2004-08-27 17:06:14 +0000

    Reorder the code, and clean up a bit.
    The ctypes.com samples are now handled as data_files instead of
    packages - this removes the warning that the __init__.py files are
    missing.
    
    --HG--
    branch : ctypes
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk/ctypes%4048471

--- setup.py
+++ setup.py
@@ -28,68 +28,11 @@ from distutils.command import install_data
 from distutils.dir_util import mkpath
 from distutils.util import get_platform
 
-kw = {}
-kw["sources"] = ["source/_ctypes.c",
-                 "source/callbacks.c",
-                 "source/callproc.c",
-                 "source/stgdict.c",
-                 "source/cfield.c"]
-
-# Support the distutils 'depends' option, if available
-if hasattr(distutils.core, 'get_distutil_options'):
-    # Python 2.3a1
-    kw["depends"] = ["source/ctypes.h"]
-elif (hasattr(distutils.core, 'extension_keywords') and 
-      'depends' in distutils.core.extension_keywords):
-    # Python current CVS
-    kw["depends"] = ["source/ctypes.h"]
-
-
-if os.name == "nt":
-    kw["sources"].extend([
-        # types.c is no longer needed, ffi_type defs are in cfield.c
-        "source/libffi_msvc/ffi.c",
-        "source/libffi_msvc/prep_cif.c",
-        "source/libffi_msvc/win32.c",
-        ])
-    extensions = [Extension("_ctypes",
-                            export_symbols=["DllGetClassObject,PRIVATE",
-                                            "DllCanUnloadNow,PRIVATE"],
-                            libraries=["ole32", "user32", "oleaut32"],
-                            include_dirs=["source/libffi_msvc"],
-                            **kw),
-                  Extension("_ctypes_test",
-                            libraries=["oleaut32"],
-                            sources=["source/_ctypes_test.c"],
-                            include_dirs=["source/libffi_msvc"],
-                            )
-                  ]
-    if kw.has_key("depends"):
-        kw["depends"].extend(["source/libffi_msvc/ffi.h",
-                              "source/libffi_msvc/fficonfig.h",
-                              "source/libffi_msvc/ffitarget.h",
-                              "source/libffi_msvc/ffi_common.h"])
-else:
-    include_dirs = []
-    library_dirs = []
-    extra_link_args = []
-    if sys.platform == "darwin":
-        kw["sources"].append("source/darwin/dlfcn_simple.c")
-        extra_link_args.extend(['-read_only_relocs', 'warning'])
-        include_dirs.append("source/darwin")
-
-    extensions = [Extension("_ctypes",
-                            libraries=["ffi"],
-                            include_dirs=include_dirs,
-                            library_dirs=library_dirs,
-                            extra_link_args=extra_link_args,
-                            **kw),
-                  Extension("_ctypes_test",
-                            sources=["source/_ctypes_test.c"])
-                  ]
 ################################################################
-# Parts of this section copied from the PyObjC project
+# Manipulate the environment for the build process.
+#
 if sys.platform == 'darwin':
+    # This section copied from the PyObjC project
     # Apple has used build options that don't work with a 'normal' system.
     # Remove '-arch i386' from the LDFLAGS.
     import distutils.sysconfig
@@ -100,43 +43,12 @@ if sys.platform == 'darwin':
         print "Fixing Apple strangeness in Python configuration"
         distutils.sysconfig._config_vars['LDSHARED'] = y
 
-##if sys.platform != 'darwin' and os.path.exists('/usr/include/ffi.h') \
-##       or os.path.exists('/usr/local/include/ffi.h'):
-##    # A system with a pre-existing libffi.
-##    LIBFFI_SOURCES=None
-
-if sys.platform == 'win32':
-    LIBFFI_SOURCES=None
-
-platform = get_platform()
-
-if platform in ["solaris-2.9-sun4u", "linux-x86_64"]:
+if get_platform() in ["solaris-2.9-sun4u", "linux-x86_64"]:
     os.environ["CFLAGS"] = "-fPIC"
 
 ################################################################
-
-packages = ["ctypes"]
-package_dir = {}
-
-if sys.platform == "win32":
-    packages.append("ctypes.com")
-    package_dir["ctypes.com"] = "win32/com"
-    packages.append("ctypes.com.samples")
-    package_dir["ctypes.com.samples"] = "win32/com/samples"
-    packages.append("ctypes.com.tools")
-    package_dir["ctypes.com.tools"] = "win32/com/tools"
-
-    packages.append("ctypes.com.samples.server")
-    package_dir["ctypes.com.samples.server"] = "win32/com/samples/server"
-
-    packages.append("ctypes.com.samples.server.control")
-    package_dir["ctypes.com.samples.server.control"] = "win32/com/samples/server/control"
-
-    packages.append("ctypes.com.samples.server.IExplorer")
-    package_dir["ctypes.com.samples.server.IExplorer"] = "win32/com/samples/server/IExplorer"
-
-################################################################
-
+# Additional and overridden distutils commands
+#
 class test(Command):
     # Original version of this class posted
     # by Berthold Hoellmann to distutils-sig@python.org
@@ -231,13 +143,15 @@ class test(Command):
                 print >> sys.stderr
 
             print >> sys.stderr, "-" * 70
-            print >> sys.stderr, "Ran %d tests in %.3fs" % (len(self.testcases), stop_time - start_time)
+            print >> sys.stderr, "Ran %d tests in %.3fs" % (len(self.testcases),
+                                                            stop_time - start_time)
             print >> sys.stderr
             if self.fail + self.errors == 0:
                 print >> sys.stderr, "OK"
             else:
                 if self.errors:
-                    print >> sys.stderr, "FAILED (failures=%d, errors=%d)" % (self.fail, self.errors)
+                    print >> sys.stderr, "FAILED (failures=%d, errors=%d)" % (self.fail,
+                                                                              self.errors)
                 else:
                     print >> sys.stderr, "FAILED (failures=%d)" % self.fail
 
@@ -291,18 +205,6 @@ class test(Command):
 
 # class test
 
-if (hasattr(distutils.core, 'setup_keywords') and 
-    'classifiers' in distutils.core.setup_keywords):
-        kw['classifiers'] = \
-                 ['Topic :: Software Development',
-                  'Operating System :: MacOS :: MacOS X',
-                  'Operating System :: Microsoft :: Windows',
-                  'Operating System :: POSIX',
-                  'Intended Audience :: Developers',
-                  'Development Status :: 3 - Alpha',
-                  'Development Status :: 4 - Beta',
-                  ]
-
 class my_build_py(build_py.build_py):
     def find_package_modules (self, package, package_dir):
         """We extend distutils' build_py.find_package_modules() method
@@ -316,8 +218,6 @@ class my_build_py(build_py.build_py):
                 result.append(('ctypes', modname, pathname))
         return result
 
-################################################################
-
 def find_file_in_subdir(dirname, filename):
     # if <filename> is in <dirname> or any subdirectory thereof,
     # return the directory name, else None
@@ -327,6 +227,7 @@ def find_file_in_subdir(dirname, filename):
     return None
 
 class my_build_ext(build_ext.build_ext):
+    # First build a static libffi library, then build the _ctypes extension.
     def run(self):
         self.build_libffi_static()
         build_ext.build_ext.run(self)
@@ -350,6 +251,8 @@ class my_build_ext(build_ext.build_ext):
 	return 1
 
     def build_libffi_static(self):
+        if sys.platform == "win32":
+            return
         if LIBFFI_SOURCES == None:
             return
         src_dir = os.path.abspath(LIBFFI_SOURCES)
@@ -388,20 +291,6 @@ class my_clean(clean.clean):
         self.build_temp = self.build_temp.replace(" ", "")
         clean.clean.run(self)
 
-# Use different MANIFEST templates, to minimize the distribution size.
-# Also, the MANIFEST templates behave differently on Windows and Linux (distutils bug?)
-options = {}
-
-if sys.platform == 'win32':
-    options["sdist"] = {"template": "MANIFEST.windows.in", "force_manifest": 1}
-    data_files = [("ctypes/com/samples/server/control",
-                   ["win32/com/samples/server/control/test.html"])
-                  ]
-
-else:
-    options["sdist"] = {"template": "MANIFEST.other.in", "force_manifest": 1}
-    data_files = []
-
 class my_install_data(install_data.install_data):
      """A custom install_data command, which will install it's files
      into the standard directories (normally lib/site-packages).
@@ -413,12 +302,142 @@ class my_install_data(install_data.install_data):
          print 'Installing data files to %s' % self.install_dir
          install_data.install_data.finalize_options(self)
 
+################################################################
+# Specify the _ctypes extension
+#
+kw = {}
+# common source files
+kw["sources"] = ["source/_ctypes.c",
+                 "source/callbacks.c",
+                 "source/callproc.c",
+                 "source/stgdict.c",
+                 "source/cfield.c"]
+
+# common header file
+kw["depends"] = ["source/ctypes.h"]
+
+if sys.platform == "win32":
+    kw["sources"].extend([
+        # types.c is no longer needed, ffi_type defs are in cfield.c
+        "source/libffi_msvc/ffi.c",
+        "source/libffi_msvc/prep_cif.c",
+        "source/libffi_msvc/win32.c",
+        ])
+    extensions = [Extension("_ctypes",
+                            export_symbols=["DllGetClassObject,PRIVATE",
+                                            "DllCanUnloadNow,PRIVATE"],
+                            libraries=["ole32", "user32", "oleaut32"],
+                            include_dirs=["source/libffi_msvc"],
+                            **kw),
+                  Extension("_ctypes_test",
+                            libraries=["oleaut32"],
+                            sources=["source/_ctypes_test.c"],
+                            include_dirs=["source/libffi_msvc"],
+                            )
+                  ]
+    if kw.has_key("depends"):
+        kw["depends"].extend(["source/libffi_msvc/ffi.h",
+                              "source/libffi_msvc/fficonfig.h",
+                              "source/libffi_msvc/ffitarget.h",
+                              "source/libffi_msvc/ffi_common.h"])
+else:
+    include_dirs = []
+    library_dirs = []
+    extra_link_args = []
+    if sys.platform == "darwin":
+        kw["sources"].append("source/darwin/dlfcn_simple.c")
+        extra_link_args.extend(['-read_only_relocs', 'warning'])
+        include_dirs.append("source/darwin")
+
+    extensions = [Extension("_ctypes",
+                            libraries=["ffi"],
+                            include_dirs=include_dirs,
+                            library_dirs=library_dirs,
+                            extra_link_args=extra_link_args,
+                            **kw),
+                  Extension("_ctypes_test",
+                            sources=["source/_ctypes_test.c"])
+                  ]
+################################################################
+# the ctypes package
+#
+packages = ["ctypes"]
+package_dir = {}
+
+################################################################
+# the ctypes.com package
+#
+if sys.platform == "win32":
+    packages.append("ctypes.com")
+    package_dir["ctypes.com"] = "win32/com"
+
+    packages.append("ctypes.com.tools")
+    package_dir["ctypes.com.tools"] = "win32/com/tools"
+
+################################################################
+# options for distutils, and ctypes.com samples
+#
+setup_options = {}
+
+if sys.platform == 'win32':
+    # Use different MANIFEST templates, to minimize the distribution
+    # size.  Also, the MANIFEST templates behave differently on
+    # Windows and Linux (distutils bug?).
+    # Finally, force rebuilding the MANIFEST file
+
+    setup_options["sdist"] = {"template": "MANIFEST.windows.in", "force_manifest": 1}
+
+    import glob
+    data_files = [("ctypes/com/samples",
+                   glob.glob("win32/com/samples/*.py") +
+                   glob.glob("win32/com/samples/*.txt")),
+
+                  ("ctypes/com/samples/server",
+                   glob.glob("win32/com/samples/server/*.py") +
+                   glob.glob("win32/com/samples/server/*.txt")),
+
+                  ("ctypes/com/samples/server/control",
+                   glob.glob("win32/com/samples/server/control/*.py") +
+                   glob.glob("win32/com/samples/server/control/*.txt") +
+                   glob.glob("win32/com/samples/server/control/*.html")),
+
+                  ("ctypes/com/samples/server/IExplorer",
+                   glob.glob("win32/com/samples/server/IExplorer/*.py") +
+                   glob.glob("win32/com/samples/server/IExplorer/*.txt") +
+                   glob.glob("win32/com/samples/server/IExplorer/*.html")),
+                  ]
+
+else:
+    setup_options["sdist"] = {"template": "MANIFEST.other.in", "force_manifest": 1}
+    data_files = []
+
+################################################################
+# pypi classifiers
+#
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: MacOS :: MacOS X',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: POSIX',
+    'Programming Language :: C',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+
+################################################################
+# main section
+#
 if __name__ == '__main__':
     setup(name="ctypes",
           ext_modules = extensions,
           package_dir = package_dir,
           packages = packages,
           data_files = data_files,
+
+          classifiers = classifiers,
 
           version=__version__,
           description="create and manipulate C data types in Python",
@@ -431,7 +450,7 @@ if __name__ == '__main__':
           
           cmdclass = {'test': test, 'build_py': my_build_py, 'build_ext': my_build_ext,
                       'clean': my_clean, 'install_data': my_install_data},
-          options = options
+          options = setup_options
           )
 
 ## Local Variables:
