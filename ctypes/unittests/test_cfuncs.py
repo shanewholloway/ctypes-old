aab0a4db79fe30aa44ed01da7bf8acf3b16a815e
theller <none@none>
2004-08-27 18:03:35 +0000

    Enlarge the numbers the tests use, for better error detection.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4048473

--- ctypes/unittests/test_cfuncs.py
+++ ctypes/unittests/test_cfuncs.py
@@ -20,96 +20,96 @@ class CFunctions(unittest.TestCase):
     def test_byte(self):
         self.dll.tf_b.restype = c_byte
         self.dll.tf_b.argtypes = (c_byte,)
-        self.failUnlessEqual(self.dll.tf_b(-42), -42)
+        self.failUnlessEqual(self.dll.tf_b(-126), -42)
         self.dll.tf_bb.restype = c_byte
         self.dll.tf_bb.argtypes = (c_byte, c_byte)
-        self.failUnlessEqual(self.dll.tf_bb(0, -42), -42)
+        self.failUnlessEqual(self.dll.tf_bb(0, -126), -42)
 
     def test_ubyte(self):
         self.dll.tf_B.restype = c_ubyte
         self.dll.tf_B.argtypes = (c_ubyte,)
-        self.failUnlessEqual(self.dll.tf_B(42), 42)
+        self.failUnlessEqual(self.dll.tf_B(255), 85)
         self.dll.tf_bB.restype = c_ubyte
         self.dll.tf_bB.argtypes = (c_byte, c_ubyte)
-        self.failUnlessEqual(self.dll.tf_bB(0, 42), 42)
+        self.failUnlessEqual(self.dll.tf_bB(0, 255), 85)
 
     def test_short(self):
         self.dll.tf_h.restype = c_short
-        self.failUnlessEqual(self.dll.tf_h(-42), -42)
+        self.failUnlessEqual(self.dll.tf_h(-32766), -10922)
         self.dll.tf_bh.restype = c_short
-        self.failUnlessEqual(self.dll.tf_bh(0, -42), -42)
+        self.failUnlessEqual(self.dll.tf_bh(0, -32766), -10922)
 
     def test_ushort(self):
         self.dll.tf_H.restype = c_ushort
-        self.failUnlessEqual(self.dll.tf_H(42), 42)
+        self.failUnlessEqual(self.dll.tf_H(65535), 21845)
         self.dll.tf_bH.restype = c_ushort
-        self.failUnlessEqual(self.dll.tf_bH(0, 42), 42)
+        self.failUnlessEqual(self.dll.tf_bH(0, 65535), 21845)
 
     def test_int(self):
         self.dll.tf_i.restype = c_int
-        self.failUnlessEqual(self.dll.tf_i(-42), -42)
+        self.failUnlessEqual(self.dll.tf_i(-2147483646), -715827882)
         self.dll.tf_bi.restype = c_int
-        self.failUnlessEqual(self.dll.tf_bi(0, -42), -42)
+        self.failUnlessEqual(self.dll.tf_bi(0, -2147483646), -715827882)
 
     def test_uint(self):
         self.dll.tf_I.restype = c_uint
-        self.failUnlessEqual(self.dll.tf_I(42), 42)
+        self.failUnlessEqual(self.dll.tf_I(4294967295), 1431655765)
         self.dll.tf_bI.restype = c_uint
-        self.failUnlessEqual(self.dll.tf_bI(0, 42), 42)
+        self.failUnlessEqual(self.dll.tf_bI(0, 4294967295), 1431655765)
 
     def test_long(self):
         self.dll.tf_l.restype = c_long
         self.dll.tf_l.argtypes = (c_long,)
-        self.failUnlessEqual(self.dll.tf_l(-42), -42)
+        self.failUnlessEqual(self.dll.tf_l(-2147483646), -715827882)
         self.dll.tf_bl.restype = c_long
         self.dll.tf_bl.argtypes = (c_byte, c_long)
-        self.failUnlessEqual(self.dll.tf_bl(0, -42), -42)
+        self.failUnlessEqual(self.dll.tf_bl(0, -2147483646), -715827882)
 
     def test_ulong(self):
         self.dll.tf_L.restype = c_ulong
-        self.failUnlessEqual(self.dll.tf_L(42), 42)
+        self.failUnlessEqual(self.dll.tf_L(4294967295), 1431655765)
         self.dll.tf_bL.restype = c_ulong
-        self.failUnlessEqual(self.dll.tf_bL(0, 42), 42)
+        self.failUnlessEqual(self.dll.tf_bL(0, 4294967295), 1431655765)
 
     def test_longlong(self):
         self.dll.tf_q.restype = c_longlong
         self.dll.tf_q.argtypes = (c_longlong, )
-        self.failUnlessEqual(self.dll.tf_q(-42L), -42L)
+        self.failUnlessEqual(self.dll.tf_q(-9223372036854775806), -3074457345618258602)
         self.dll.tf_bq.restype = c_longlong
         self.dll.tf_bq.argtypes = (c_byte, c_longlong)
-        self.failUnlessEqual(self.dll.tf_bq(0, -42), -42)
+        self.failUnlessEqual(self.dll.tf_bq(0, -9223372036854775806), -3074457345618258602)
 
     def test_ulonglong(self):
         self.dll.tf_Q.restype = c_ulonglong
         self.dll.tf_Q.argtypes = (c_longlong, )
-        self.failUnlessEqual(self.dll.tf_Q(42), 42)
+        self.failUnlessEqual(self.dll.tf_Q(18446744073709551615), 6148914691236517205)
         self.dll.tf_bQ.restype = c_ulonglong
         self.dll.tf_bQ.argtypes = (c_byte, c_ulonglong)
-        self.failUnlessEqual(self.dll.tf_bQ(0, 42), 42)
+        self.failUnlessEqual(self.dll.tf_bQ(0, 18446744073709551615), 6148914691236517205)
 
     def test_float(self):
         self.dll.tf_f.restype = c_float
         self.dll.tf_f.argtypes = (c_float,)
-        self.failUnlessEqual(self.dll.tf_f(-42.), -42.)
+        self.failUnlessEqual(self.dll.tf_f(-42.), -14.)
         self.dll.tf_bf.restype = c_float
         self.dll.tf_bf.argtypes = (c_byte, c_float)
-        self.failUnlessEqual(self.dll.tf_bf(0, -42.), -42.)
+        self.failUnlessEqual(self.dll.tf_bf(0, -42.), -14.)
 
     def test_double(self):
         self.dll.tf_d.restype = c_double
         self.dll.tf_d.argtypes = (c_double,)
-        self.failUnlessEqual(self.dll.tf_d(42), 42)
+        self.failUnlessEqual(self.dll.tf_d(42.), 14.)
         self.dll.tf_bd.restype = c_double
         self.dll.tf_bd.argtypes = (c_byte, c_double)
-        self.failUnlessEqual(self.dll.tf_bd(0, 42), 42)
+        self.failUnlessEqual(self.dll.tf_bd(0, 42.), 14.)
 
     def test_callwithresult(self):
         def process_result(result):
             return result * 2
         self.dll.tf_i.restype = process_result
         self.dll.tf_i.argtypes = (c_int,)
-        self.failUnlessEqual(self.dll.tf_i(42), 84)
-        self.failUnlessEqual(self.dll.tf_i(-42), -84)
+        self.failUnlessEqual(self.dll.tf_i(42), 28)
+        self.failUnlessEqual(self.dll.tf_i(-42), -28)
 
     def test_void(self):
         self.dll.tv_i.restype = None
