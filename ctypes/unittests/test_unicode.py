8da830878ece362059c6b8cb2f8d6f66fd63549e
theller <none@none>
2004-10-12 18:03:54 +0000

    Test unicode conversions.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4048597

---
+++ ctypes/unittests/test_unicode.py
@@ -0,0 +1,100 @@
+# coding: latin-1
+import unittest
+import ctypes
+
+def get_libc():
+    import os, sys
+    if os.name == "nt":
+        return ctypes.cdll.msvcrt
+    elif os.name == "posix":
+        if sys.platform == "darwin":
+            return ctypes.cdll.LoadLibrary("/usr/lib/libc.dylib")
+        elif sys.platform == "cygwin":
+            return ctypes.cdll.LoadLibrary("/bin/cygwin1.dll")
+        elif sys.platform == "sunos5":
+            return ctypes.cdll.LoadLibrary("/lib/libc.so")
+        else:
+            try:
+                return ctypes.cdll.LoadLibrary("/lib/libc.so.6")
+            except OSError:
+                pass
+    return None
+libc = get_libc()
+wcslen = libc.wcslen
+wcslen.argtypes = [ctypes.c_wchar_p]
+
+
+class UnicodeTestCase(unittest.TestCase):
+    def setUp(self):
+        self.prev_conv_mode = ctypes.set_conversion_mode("ascii", "strict")
+
+    def tearDown(self):
+        ctypes.set_conversion_mode(*self.prev_conv_mode)
+
+    def test_ascii_strict(self):
+        ctypes.set_conversion_mode("ascii", "strict")
+        # no conversions take place with unicode arguments
+        self.failUnlessEqual(wcslen(u"abc"), 3)
+        self.failUnlessEqual(wcslen(u"ab\u2070"), 3)
+        # string args are converted
+        self.failUnlessEqual(wcslen("abc"), 3)
+        self.failUnlessRaises(ctypes.ArgumentError, wcslen, "abä")
+
+    def test_ascii_replace(self):
+        ctypes.set_conversion_mode("ascii", "replace")
+        self.failUnlessEqual(wcslen(u"abc"), 3)
+        self.failUnlessEqual(wcslen(u"ab\u2070"), 3)
+        self.failUnlessEqual(wcslen("abc"), 3)
+        self.failUnlessEqual(wcslen("abä"), 3)
+
+    def test_ascii_ignore(self):
+        ctypes.set_conversion_mode("ascii", "ignore")
+        self.failUnlessEqual(wcslen(u"abc"), 3)
+        self.failUnlessEqual(wcslen(u"ab\u2070"), 3)
+        # ignore error mode skips non-ascii characters
+        self.failUnlessEqual(wcslen("abc"), 3)
+        self.failUnlessEqual(wcslen("äöüß"), 0)
+
+    def test_latin1_strict(self):
+        ctypes.set_conversion_mode("latin-1", "strict")
+        self.failUnlessEqual(wcslen(u"abc"), 3)
+        self.failUnlessEqual(wcslen(u"ab\u2070"), 3)
+        self.failUnlessEqual(wcslen("abc"), 3)
+        self.failUnlessEqual(wcslen("äöüß"), 4)
+
+import _ctypes_test
+func = ctypes.CDLL(_ctypes_test.__file__)._testfunc_p_p
+
+class StringTestCase(UnicodeTestCase):
+    def setUp(self):
+        self.prev_conv_mode = ctypes.set_conversion_mode("ascii", "strict")
+        func.argtypes = [ctypes.c_char_p]
+        func.restype = ctypes.c_char_p
+
+    def tearDown(self):
+        ctypes.set_conversion_mode(*self.prev_conv_mode)
+        func.argtypes = None
+        func.restype = ctypes.c_int
+
+    def test_ascii_replace(self):
+        ctypes.set_conversion_mode("ascii", "strict")
+        self.failUnlessEqual(func("abc"), "abc")
+        self.failUnlessEqual(func(u"abc"), "abc")
+        self.assertRaises(ctypes.ArgumentError, func, u"abä")
+
+    def test_ascii_ignore(self):
+        ctypes.set_conversion_mode("ascii", "ignore")
+        self.failUnlessEqual(func("abc"), "abc")
+        self.failUnlessEqual(func(u"abc"), "abc")
+        self.failUnlessEqual(func(u"äöüß"), "")
+
+    def test_ascii_replace(self):
+        ctypes.set_conversion_mode("ascii", "replace")
+        self.failUnlessEqual(func("abc"), "abc")
+        self.failUnlessEqual(func(u"abc"), "abc")
+        self.failUnlessEqual(func(u"äöüß"), "????")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
