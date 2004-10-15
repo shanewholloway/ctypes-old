1eb5bd14f6675c6580b8b6878e758c9b5af78ee5
theller <none@none>
2004-10-15 16:55:50 +0000

    Add memset function.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4048635

--- ctypes/unittests/test_memfunctions.py
+++ ctypes/unittests/test_memfunctions.py
@@ -9,6 +9,16 @@ class MemFunctionsTest(unittest.TestCase):
         self.failUnlessEqual(a.value, "Hello, World")
 
         self.failUnlessEqual(get_string(result), "Hello, World")
+        self.failUnlessEqual(get_string(result, 5), "Hello")
+        self.failUnlessEqual(get_string(result, 16), "Hello, World\0\0\0\0")
+
+    def test_memset(self):
+        a = create_string_buffer(32)
+        result = memset(a, ord('x'), 16)
+
+        self.failUnlessEqual(get_string(result), "xxxxxxxxxxxxxxxx")
+        self.failUnlessEqual(get_string(a), "xxxxxxxxxxxxxxxx")
+        self.failUnlessEqual(a.value, "xxxxxxxxxxxxxxxx")
 
 if __name__ == "__main__":
     unittest.main()
