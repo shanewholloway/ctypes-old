bee66f02d5675d0ccb4d4c178b6ecc37505701f3
theller <none@none>
2004-10-20 10:35:11 +0000

    Cycle GC support for the PointerType_Type metaclass.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4048653

--- ctypes/source/_ctypes.c
+++ ctypes/source/_ctypes.c
@@ -496,6 +496,7 @@ PointerType_SetProto(StgDictObject *stgdict, PyObject *proto)
 		return -1;
 	}
 	Py_INCREF(proto);
+	Py_XDECREF(stgdict->proto);
 	stgdict->proto = proto;
 	return 0;
 }
@@ -624,10 +625,10 @@ PyTypeObject PointerType_Type = {
 	0,					/* tp_getattro */
 	0,					/* tp_setattro */
 	0,					/* tp_as_buffer */
-	Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /* tp_flags */
+	Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE | Py_TPFLAGS_HAVE_GC, /* tp_flags */
 	"metatype for the Pointer Objects",	/* tp_doc */
-	0,					/* tp_traverse */
-	0,					/* tp_clear */
+	(traverseproc)CDataType_traverse,	/* tp_traverse */
+	(inquiry)CDataType_clear,		/* tp_clear */
 	0,					/* tp_richcompare */
 	0,					/* tp_weaklistoffset */
 	0,					/* tp_iter */
