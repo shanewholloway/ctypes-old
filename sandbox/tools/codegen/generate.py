fb78f0ce069c32e15a3ab2157ce45e5a9610f527
theller <none@none>
2004-11-11 21:04:47 +0000

    ctypes_names isn't worth to be a complete module. Lots of other changes as well.
    --HG--
    branch : ctypes
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk/ctypes%4048722

--- sandbox/tools/codegen/generate.py
+++ sandbox/tools/codegen/generate.py
@@ -1,20 +1,113 @@
+# bugs:
+# packing of derived structures (assertion error at build time)
+# packing of unions (assertion errors at import time of generated module)
+# bitfields?
+
 import nodes
-from ctypes_names import ctypes_names
 
 try:
     set
 except NameError:
     from sets import Set as set
 
+# XXX Should this be in ctypes itself?
+ctypes_names = {
+    "unsigned char": "c_ubyte",
+    "signed char": "c_byte",
+    "char": "c_char",
+
+    "wchar_t": "c_wchar",
+
+    "short unsigned int": "c_ushort",
+    "short int": "c_short",
+
+    "long unsigned int": "c_ulong",
+    "long int": "c_long",
+    "long signed int": "c_long",
+
+    "unsigned int": "c_uint",
+    "int": "c_int",
+
+    "long long unsigned int": "c_ulonglong",
+    "long long int": "c_longlong",
+
+    "double": "c_double",
+    "float": "c_float",
+
+    # Hm...
+    "void": "None",
+}
+
 ################
 
-def type_name(t):
+def storage(t):
+    # return the size and alignment of a type
+    if isinstance(t, nodes.Typedef):
+        return storage(t.typ)
+    elif isinstance(t, nodes.ArrayType):
+        s, a = storage(t.typ)
+        return s * (int(t.max) - int(t.min) + 1), a
+    return int(t.size), int(t.align)
+
+class PackingError(Exception):
+    pass
+
+def _calc_packing(struct, fields, pack, verbose):
+    if struct.size is None: # incomplete struct
+        return -1
+    if struct.name in dont_assert_size:
+        return None
+    assert not isinstance(struct, nodes.Union)
+    if struct.bases:
+        size = struct.bases[0].size
+    else:
+        size = 0
+    total_align = 8 # in bits
+    for i, f in enumerate(fields):
+        if f.bits:
+            return -2
+        s, a = storage(f.typ)
+        if pack is not None:
+            a = min(pack, a)
+        if size % a:
+            size += a - size % a
+        if size != int(f.offset):
+            raise PackingError, "field offset"
+        size += s
+        total_align = max(total_align, a)
+    if total_align != struct.align:
+        raise PackingError, "total alignment (%s/%s)" % (total_align / 8, struct.align)
+    a = total_align
+    if pack is not None:
+        a = min(pack, a)
+    if size % a:
+        size += a - size % a
+    if size != struct.size:
+        raise PackingError, "total size"
+
+def calc_packing(struct, fields, verbose=False):
+    # try several packings, starting with unspecified packing
+    for pack in [None, 16*8, 8*8, 4*8, 2*8, 1*8]:
+        try:
+            _calc_packing(struct, fields, pack, verbose)
+        except PackingError:
+            continue
+        else:
+            if pack is None:
+                return None
+            return pack/8
+##    assert 0, "PACKING FAILED"
+    print "##WARNING: Packing failed", struct.size
+
+def _type_name(t):
     # Return a string, containing an expression which can be used to
     # refer to the type. Assumes the ctypes.* namespace is available.
     if isinstance(t, nodes.PointerType):
         result = "POINTER(%s)" % type_name(t.typ)
         # XXX Better to inspect t.typ!
         if result.startswith("POINTER(WINFUNCTYPE"):
+            return result[8:-1]
+        if result.startswith("POINTER(CFUNCTYPE"):
             return result[8:-1]
         # XXX See comment above...
         elif result == "POINTER(None)":
@@ -24,8 +117,8 @@ def type_name(t):
         return "%s * %s" % (type_name(t.typ), int(t.max)+1)
     elif isinstance(t, nodes.FunctionType):
         args = map(type_name, [t.returns] + t.arguments)
-        # what now?  WINFUNCTYPE already *is* a pointer to a function
-        return "WINFUNCTYPE(%s)" % ", ".join(args)
+        # WINFUNCTYPE already *is* a pointer to a function!
+        return "CFUNCTYPE(%s)" % ", ".join(args)
     elif isinstance(t, nodes.CvQualifiedType):
         return "const(%s)" % type_name(t.typ)
     elif isinstance(t, nodes.FundamentalType):
@@ -37,8 +130,16 @@ def type_name(t):
             return t.name
         return "c_int" # enums are integers
     elif isinstance(t, nodes.Typedef):
-        return type_name(get_real_type(t.typ))
+        return t.name
     return t.name
+
+renames = {
+##    "POINTER(const(WCHAR))": "c_wchar_p",
+    }
+
+def type_name(t):
+    result = _type_name(t)
+    return renames.get(result, result)
 
 def get_real_type(tp):
     if type(tp) is nodes.Typedef:
@@ -104,7 +205,7 @@ rpcrt4""".split()
 
 ##rpcndr
 ##ntdll
-
+##dll_names = "libxml2".split()
 
 from ctypes import CDLL
 searched_dlls = [CDLL(name) for name in dll_names]
@@ -158,9 +259,8 @@ class Generator(object):
     def ArrayType(self, tp):
         if tp in self.done:
             return
-        if type(tp.typ) is nodes.Typedef:
-            self.more.add(tp.typ)
         self.generate(get_real_type(tp.typ))
+        self.generate(tp.typ)
         self.done.add(tp)
 
     def FunctionType(self, tp):
@@ -194,9 +294,13 @@ class Generator(object):
         if tp in self.done:
             return
         if tp.name:
+            print
             print "%s = c_int # enum" % tp.name
-        for n, v in tp.values:
-            print "%s = %s # enum" % (n, v)
+            for n, v in tp.values:
+                print "%s = %s # enum %s" % (n, v, tp.name)
+        else:
+            for n, v in tp.values:
+                print "%s = %s # enum" % (n, v)
         self.done.add(tp)
 
     def StructureBody(self, body):
@@ -208,20 +312,25 @@ class Generator(object):
             if type(m) is nodes.Field:
                 fields.append(m)
                 if type(m.typ) is nodes.Typedef:
-                    self.more.add(m.typ)
-                self.generate(get_real_type(m.typ))
+                    self.generate(get_real_type(m.typ))
+                self.generate(m.typ)
             elif type(m) is nodes.Method:
                 methods.append(m)
                 self.generate(m.returns)
                 self.generate_all(m.arguments)
             elif type(m) is nodes.Constructor:
                 pass
-        try:
-            pack = struct_packing[body.struct.name]
-        except KeyError:
-            pass
-        else:
-            print "%s._pack_ = %s" % (body.struct.name, pack)
+
+        # we don't need _pack_ on Unions (I hope, at least), and
+        # not on COM interfaces:
+        #
+        # Hm, how to detect a COM interface with no methods? IXMLDOMCDATASection is such a beast...
+        if not isinstance(body.struct, nodes.Union) and not methods:
+            pack = calc_packing(body.struct, fields)
+            if pack is not None:
+                print "%s._pack_ = %s" % (body.struct.name, pack)
+            else:
+                print "# %s" % body.struct.name
         if fields:
             if body.struct.bases:
                 assert len(body.struct.bases) == 1
@@ -249,6 +358,9 @@ class Generator(object):
             size = body.struct.size // 8
             print "assert sizeof(%s) == %s, sizeof(%s)" % \
                   (body.struct.name, size, body.struct.name)
+            align = body.struct.align // 8
+            print "assert alignment(%s) == %s, alignment(%s)" % \
+                  (body.struct.name, align, body.struct.name)
         self.done.add(body)
 
     def find_dllname(self, name):
@@ -259,7 +371,8 @@ class Generator(object):
                 pass
             else:
                 return dll._name
-        return None
+##        return None
+        return "?"
 
     def Function(self, func):
         if func in self.done:
@@ -270,10 +383,10 @@ class Generator(object):
             self.generate_all(func.arguments)
             args = [type_name(a) for a in func.arguments]
             if "__stdcall__" in func.attributes:
-                print "%s = STDCALL('%s', %s, '%s', (%s))" % \
+                print "%s = STDCALL('%s', %s, '%s', [%s])" % \
                       (func.name, dllname, type_name(func.returns), func.name, ", ".join(args))
             else:
-                print "%s = CDECL('%s', %s, '%s', (%s))" % \
+                print "%s = CDECL('%s', %s, '%s', [%s])" % \
                       (func.name, dllname, type_name(func.returns), func.name, ", ".join(args))
         self.done.add(func)
 
@@ -296,7 +409,10 @@ class Generator(object):
 
 def find_names(names):
     from gccxmlparser import parse
-    items = parse(files=["windows.h"], xmlfile="windows.xml", options=["-D _WIN32_WINNT=0x500"])
+    items = parse(files=["windows.h"],
+                  options=["-D WIN32_LEAN_AND_MEAN"],
+##                  options=["-dM"],
+                  xmlfile="windows.xml")
 
     if "*" in names:
         return items
@@ -308,10 +424,11 @@ def find_names(names):
     return result
 
 def main():
-    from gccxmlparser import parse
 ##    items = parse(files=["windows.h"], xmlfile="windows.xml")
     items = find_names(sys.argv[1:])
     gen = Generator()
+    print "# files='windows.h'"
+    print "# options='-D WIN32_LEAN_AND_MEAN'"
     print "from ctypes import *"
     print "from _support import STDMETHOD, const, STDCALL, CDECL, _com_interface"
 ##    print "def STDMETHOD(*args): pass"
@@ -335,6 +452,7 @@ def main():
 if __name__ == "__main__":
     import sys
     if len(sys.argv) == 1:
-        sys.argv.extend("WNDCLASS".split())
+        sys.argv.extend("*")
+#        sys.argv.extend("WNDCLASS".split())
 ##        sys.argv.extend("ITypeComp".split())
     main()
