2d82e47f7798bda8148b3f210023531ac974ce9f
theller <none@none>
2004-12-02 12:56:16 +0000

    Added --dll and --windows-dlls options.
    --HG--
    branch : ctypes
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk/ctypes%4048769

--- sandbox/tools/codegen/xml2py.py
+++ sandbox/tools/codegen/xml2py.py
@@ -5,38 +5,44 @@ from codegenerator import generate_code
 
 ################################################################
 
-def main():
+def main(args=None):
+    if args is None:
+        args = sys.argv
     from optparse import OptionParser
 
+    def windows_dlls(option, opt, value, parser):
+        parser.values.dlls.extend("kernel32 gdi32 user32".split())
+
     parser = OptionParser("usage: %prog [options] xmlfile")
+    parser.add_option("--windows-dlls",
+                      action="callback",
+                      callback=windows_dlls,
+                      help="add all standard windows dlls")
+    parser.add_option("--dll",
+                      dest="dlls",
+                      action="append",
+                      default=[])
     parser.add_option("-s",
                       dest="symbols",
                       help="comma separated list of symbols to include "
                       "(if not specified, all symbols will be included)",
                       default=None)
     parser.add_option("-o",
-                      dest="python_file",
+                      dest="output",
                       help="output filename (if not specified, standard output will be used)",
                       default="-")
-    options, files = parser.parse_args()
+    options, files = parser.parse_args(args[1:])
 
     if len(files) != 1:
-        parser.error("Only one input file can be specified")
+        parser.error("Exactly one input file must be specified")
 
-    if options.python_file == "-":
+    if options.output == "-":
         stream = sys.stdout
     else:
-        stream = open(options.python_file, "w")
+        stream = open(options.output, "w")
 
     generate_code(files[0], stream, symbols=options.symbols)
 
 
 if __name__ == "__main__":
-    import sys
-    if len(sys.argv) == 1:
-##        sys.argv.append("win32.xml")
-##        sys.argv.append("-sCoCreateInstance")
-        sys.argv.append("win32.xml")
-        sys.argv.append("-sCLSIDFromString,StringFromGUID2,IsEqualGUID")
-##        sys.argv.append("-owin32.py")
-    main()
+    sys.exit(main())
