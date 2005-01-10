a1f77c2fec95ef8d9f2127b59db9f096431058e3
theller <none@none>
2005-01-10 16:24:29 +0000

    Isolate windows specific code.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4048822

--- ctypes/sandbox/tools/codegen/h2xml.py
+++ ctypes/sandbox/tools/codegen/h2xml.py
@@ -1,21 +1,23 @@
 """h2xml - convert C include file(s) into an xml file by running gccxml."""
 import sys, os, tempfile
-import _winreg
 
-def _locate_gccxml():
-    for subkey in (r"Software\gccxml", r"Software\Kitware\GCC_XML"):
-        for root in (_winreg.HKEY_CURRENT_USER, _winreg.HKEY_LOCAL_MACHINE):
-            try:
-                hkey = _winreg.OpenKey(root, subkey, 0, _winreg.KEY_READ)
-            except WindowsError, detail:
-                if detail.errno != 2:
-                    raise
-            else:
-                return _winreg.QueryValueEx(hkey, "loc")[0] + r"\bin"
+if sys.platform == "win32":
 
-loc = _locate_gccxml()
-if loc:
-    os.environ["PATH"] = loc
+    def _locate_gccxml():
+        import _winreg
+        for subkey in (r"Software\gccxml", r"Software\Kitware\GCC_XML"):
+            for root in (_winreg.HKEY_CURRENT_USER, _winreg.HKEY_LOCAL_MACHINE):
+                try:
+                    hkey = _winreg.OpenKey(root, subkey, 0, _winreg.KEY_READ)
+                except WindowsError, detail:
+                    if detail.errno != 2:
+                        raise
+                else:
+                    return _winreg.QueryValueEx(hkey, "loc")[0] + r"\bin"
+
+    loc = _locate_gccxml()
+    if loc:
+        os.environ["PATH"] = loc
 
 ################################################################
 
