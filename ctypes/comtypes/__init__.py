f398a9213672274cee1022b1dfeb659ad2085e56
theller <none@none>
2005-03-04 09:40:44 +0000

    It's overkill to have a separate file for BSTR.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4049003

--- ctypes/comtypes/__init__.py
+++ ctypes/comtypes/__init__.py
@@ -185,9 +185,23 @@ class _compointer_base(c_void_p):
 
     def __repr__(self):
         return "<%s instance at %x>" % (self.__class__.__name__, id(self))
-################################################################
 
-from comtypes.BSTR import BSTR
+################################################################
+# Memory mamagement of BSTR is broken.
+#
+# The way we do them here, it is not possible to transfer the
+# ownership of a BSTR instance.  ctypes allocates the memory with
+# SysAllocString if we call the constructor with a string, and the
+# instance calls SysFreeString when it is destroyed.
+# So BSTR's received from dll function calls will never be freed,
+# and BSTR's we pass to functions are freed too often ;-(
+
+from ctypes import _SimpleCData
+
+class BSTR(_SimpleCData):
+    _type_ = "X"
+    def __repr__(self):
+        return "%s(%r)" % (self.__class__.__name__, self.value)
 
 def STDMETHOD(restype, name, argtypes=()):
     "Specifies a COM method slot"
