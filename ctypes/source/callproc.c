cbc878c2ac647f020d5ec530a69a51e3a5d92e35
theller <none@none>
2005-03-10 08:53:33 +0000

    Document some internals about function calls.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4049029

--- ctypes/source/callproc.c
+++ ctypes/source/callproc.c
@@ -15,55 +15,43 @@
 /*
   How are functions called, and how are parameters converted to C ?
 
-  XXX to be rewritten..., no longer what happens!
+  1. _ctypes.c::CFuncPtr_call receives an argument tuple 'inargs' and a
+  keyword dictionary 'kwds'.
 
-   1. (in CFuncPtr_call) restype and converters are got from self or stgdict.
-   2. If it's a COM method, the COM pointer is retrieved from the argument list.
-   3. If converters are there, the number of arguments is checked.
-   4. CallProc is called, for a COM method with a slice [1:-1] of the arg list.
+  2. After several checks, _build_callargs() is called which returns another
+  tuple 'callargs'.  This may be the same tuple as 'inargs', a slice of
+  'inargs', or a completely fresh tuple, depending on several things (is is a
+  COM method, are 'paramflags' available).
 
-   5. An array of PyCArgObject pointers is allocated to hold all the converted arguments.
-   6. If converters are present, each argument is replaced by the result of passing the argument
-      to the converter.
-   7. Each argument is passed to ConvParam, which creates a PyCArgObject.
-   8. Another PyCArgObject is allocated to hold the result of the function call.
-   9. PrepareResult() is called with the restype to fill out the tag field of the PyCArgObject.
+  3. If [out] parameters are present in paramflags, _build_callargs also
+  creates and returns another object, which is either a single object or a
+  tuple, whcih will later be used to build the function return value.
 
-  10. _call_function_pointer is called.
+  4. _CallProc is then called with the 'callargs' tuple.  _CallProc first
+  allocates two arrays.  The first is an array of 'struct argument' items, the
+  second array has 'void *' entried.
 
-  11. GetResult() is called with the 'result' PyCArgObject' to convert the C data
-      into a Python object.
-  12. All the PyCArgObjects are DECREF'd.
+  5. If 'converters' are present (converters is a sequence of argtypes'
+  from_param methods), for each item in 'callargs' converter is called and the
+  result passed to ConvParam.  If 'converters' are not present, each argument
+  is directly passed to ConvParm.
 
-  What does PrepareResult and GetResult do?
+  6. For each arg, ConvParam stores the contained C data (or a pointer to it,
+  for structures) into the 'struct argument' array.
 
-  PrepareResult sets the 'tag' field of the PyCArgObject.
+  7. Finally, a loop fills the 'void *' array so that each item points to the
+  data contained in or pointed to by the 'struct argument' array.
 
-    - if no restype, assume integer, and set it to 'i'.
-    - get the type's stgdict, if it has a getfunc and a string proto,
-      set the tag to the format character (if it is in "bBhHiIlLqQdfP")
-    - if it is a ctypes Pointer type, set it to 'P'
-    - if it is callable, set it to 'i' (and later call it with the integer result)
-    - otherwise assume integer, and set it to 'i'
-  
-  _call_function_pointer iterates over the PyCArgObject array, and pushed their values
-  onto the stack, C type depending on the 'tag'.
-  Depending if the result PyCArgObject's 'tag', the function is called and the result
-  stored into the result's space.
+  8. The 'void *' argument array is what _call_function_pointer
+  expects. _call_function_pointer then has very little to do - only some
+  libffi specific stuff, then it calls ffi_call.
 
-  The libffi version of _call_function_pointer creates an array of ffi_type pointers,
-  and fills it with ffi_type_xxx depending on the PyCArgObject's tag value.
-  Same for the PyCArgObject's result tag, then calls the ffi_call function.
+  So, there are 4 data structures holding processed arguments:
+  - the inargs tuple (in CFuncPtr_call)
+  - the callargs tuple (in CFuncPtr_call)
+  - the 'struct argguments' array
+  - the 'void *' array
 
-  ffi_call needs an array of ffi_type pointers, and an array of void* pointers
-  pointing to the argument values (the value field of the PyCArgObject instances).
-
-  GetResult doesn't use the 'tag' field, it examines restype again:
-
-    - If it is a ctypes pointer: create an empty instance, and memcpy() thze result into it.
-    - If the restype's stgdict has a getfunc, call it.
-    - If the restype is callable, call it with the integer result
-  
  */
 
 #include "Python.h"
