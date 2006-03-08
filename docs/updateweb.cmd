0875bca5da70eca62d9180648c185f992e28e39f
theller <none@none>
2006-03-08 19:04:16 +0000

    Remove out-of-date docs.
    --HG--
    branch : ctypes
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk/ctypes%4050089

--- docs/updateweb.cmd
+++
@@ -1,3 +0,0 @@
-REM $Id$
-make_html -w
-scp ../LICENSE.txt default.css changes.html com.html faq.html index.html sum_sample.html tutorial.html reference.html theller@starship.python.net:~/public_html/ctypes/
