ef2abb37ccbe70e64ffd6a12c486dca74e5c4df8
theller <none@none>
2006-04-13 19:58:08 +0000

    Update the tutorial, and ensure that it works on linux.
    --HG--
    branch : ctypes
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk/ctypes%4050255

--- docs/manual/test-tutorial.py
+++ docs/manual/test-tutorial.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python
+import sys
 import doctest
+WINDOWS = doctest.register_optionflag("WINDOWS")
+LINUX = doctest.register_optionflag("LINUX")
 SKIP = doctest.register_optionflag("SKIP")
+
+import ctypes
+c_int_name = ctypes.c_int.__name__
 
 base = doctest.DocTestRunner
 class MyDocTestRunner(base):
     def run(self, test, compileflags=None, out=None, clear_globs=True):
         examples = test.examples[:]
         for ex in test.examples:
-            if SKIP in ex.options:
+            if WINDOWS in ex.options and sys.platform != "win32":
                 examples.remove(ex)
+            elif LINUX in ex.options and not sys.platform.startswith("linux"):
+                examples.remove(ex)
+            elif SKIP in ex.options:
+                examples.remove(ex)
+            else:
+##                print "REPLACE:"
+##                print "\t", ex.want
+##                print "\t", ex.want.replace("c_long", c_int_name)
+                ex.want = ex.want.replace("c_long", c_int_name)
         test.examples = examples
         return base.run(self, test, compileflags, out, clear_globs)
 doctest.DocTestRunner = MyDocTestRunner
