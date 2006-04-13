601bec49d1744089066f87ed66865f04596690a0
theller <none@none>
2006-04-13 18:08:19 +0000

    *** empty log message ***
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4050244

--- ctypes/test-cf.py
+++ ctypes/test-cf.py
@@ -22,16 +22,17 @@ hostinfo = [
      "~/netbsd/bin/python2.4 -u"),
 
     ("amd64-linux1", "/usr/bin/python -u"),
-    ("amd64-linux2", "/usr/bin/python -u"),
+    # offline
+##    ("amd64-linux2", "/usr/bin/python -u"),
 
     # Host key verification failed
     ("alpha-linux1", "/usr/bin/python -u"),
 
     # No route to host:
-    ("ppc-osx1", "/sw/bin/python -u"),
+##    ("ppc-osx1", "/sw/bin/python -u"),
 
     # No route to host:
-    ("ppc-osx2", "/usr/bin/python -u"),
+##    ("ppc-osx2", "/usr/bin/python -u"),
 
     # Not python 2.3 or newer, so use my own build of Python 2.4.2:
     ("sparc-solaris1", # "/usr/local/bin/python -u"
@@ -43,14 +44,24 @@ hostinfo = [
 
 import sys, os
 
-for hostname, python in hostinfo:
+try:
+    from others import others
+except ImportError:
+    others = []
+
+for hostname, python in others + hostinfo:
     print "*" * 20, hostname, "*" * 20
     if len(sys.argv) > 1:
-        cmd = "cd ~/ctypes; %s setup.py %s" % (python, " ".join(sys.argv[1:]))
+        args = " ".join(sys.argv[1:])
     else:
-        cmd = "cd ~/ctypes; %s setup.py test " % python
+        args = "test"
+
+    cmd = "cd $HOME/ctypes; %s setup.py %s" % (python, args)
     print cmd
-    ret = os.system('ssh -l theller cf-shell.sf.net "ssh %s \'%s\'"' % (hostname, cmd))
+    if (hostname, python) in hostinfo:
+        ret = os.system('ssh -l theller cf-shell.sf.net "ssh %s \'%s\'"' % (hostname, cmd))
+    else:
+        ret = os.system('ssh -l theller %s \"%s\"' % (hostname, cmd))
     if ret:
         print "(RETCODE %s)" % ret
     print "-" * 50
