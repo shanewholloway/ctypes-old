0c13c6becdee08cc3332e507baaded9c3a40d831
theller <none@none>
2006-04-18 20:18:31 +0000

    Patch from Skip Montanaro, in Python SVN: C++ compiler cleanup: the typical few casts, and ...  C++ didn't like that the StgDictObject's ffi_type member had the same name as its type.  I changed that to ffi_type_pointer.  Feel free to change it to something else more meaningful, just not ffi_type.
    Fix refcount issues in CreateSwappedType - this removes the refleak
    when reloading ctypes.
    
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4050264

--- ctypes/source/callproc.c
+++ ctypes/source/callproc.c
@@ -581,14 +581,14 @@ ffi_type *GetType(PyObject *obj)
 	/* This little trick works correctly with MSVC.
 	   It returns small structures in registers
 	*/
-	if (dict->ffi_type.type == FFI_TYPE_STRUCT) {
-		if (dict->ffi_type.size <= 4)
+	if (dict->ffi_type_pointer.type == FFI_TYPE_STRUCT) {
+		if (dict->ffi_type_pointer.size <= 4)
 			return &ffi_type_sint32;
-		else if (dict->ffi_type.size <= 8)
+		else if (dict->ffi_type_pointer.size <= 8)
 			return &ffi_type_sint64;
 	}
 #endif
-	return &dict->ffi_type;
+	return &dict->ffi_type_pointer;
 }
 
 
