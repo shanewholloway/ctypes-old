3c76bf140942bbb6d46371f80aeeb0485795ab83
theller <none@none>
2006-05-31 17:55:21 +0000

    *** empty log message ***
    --HG--
    branch : ctypes
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk/ctypes%4050348

--- docs/manual/make.bat
+++ docs/manual/make.bat
@@ -1,3 +1,3 @@
-c:\python24\scripts\rst2html.py manual.txt manual.html
-c:\python24\scripts\rst2html.py docs.txt docs.html
-c:\python24\scripts\rst2html.py tutorial.txt tutorial.html
+filter.py tutorial.txt | c:\python24\python c:\python24\scripts\rst2html.py > tutorial.html
+filter.py reference.txt | c:\python24\python c:\python24\scripts\rst2html.py > reference.html
+filter.py ctypes.txt | c:\python24\python c:\python24\scripts\rst2html.py > ctypes.html
