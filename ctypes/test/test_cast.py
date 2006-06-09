0fad59067c2cf336240b0c4a167d039c91e33bdd
theller <none@none>
2006-06-09 18:07:16 +0000

    Adapt test to the way cast works now.
    Minial test for pointer item assignments with index != 0.
    
    --HG--
    branch : ctypes
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk/ctypes%4050390

--- ctypes/test/test_cast.py
+++ ctypes/test/test_cast.py
@@ -39,14 +39,23 @@ class Test(unittest.TestCase):
         p = cast(array, POINTER(c_char_p))
         # array and p share a common _objects attribute
         self.failUnless(p._objects is array._objects)
-        self.failUnlessEqual(array._objects, {'0': "foo bar"})
+        self.failUnlessEqual(array._objects, {'0': "foo bar", id(array): array})
         p[0] = "spam spam"
-        self.failUnlessEqual(p._objects, {'0': "spam spam"})
-        self.failUnlessEqual(array._objects, {'0': "spam spam"})
+        self.failUnlessEqual(p._objects, {'0': "spam spam", id(array): array})
+        self.failUnless(array._objects is p._objects)
         p[1] = "foo bar"
-        self.failUnlessEqual(p._objects, {'1': 'foo bar', '0': "spam spam"})
-        self.failUnlessEqual(array._objects, {'1': 'foo bar', '0': "spam spam"})
+        self.failUnlessEqual(p._objects, {'1': 'foo bar', '0': "spam spam", id(array): array})
+        self.failUnless(array._objects is p._objects)
 
+    def test_other(self):
+        p = cast((c_int * 4)(1, 2, 3, 4), POINTER(c_int))
+        self.failUnlessEqual(p[:4], [1,2, 3, 4])
+        c_int()
+        self.failUnlessEqual(p[:4], [1, 2, 3, 4])
+        p[2] = 96
+        self.failUnlessEqual(p[:4], [1, 2, 96, 4])
+        c_int()
+        self.failUnlessEqual(p[:4], [1, 2, 96, 4])
 
 if __name__ == "__main__":
     unittest.main()
