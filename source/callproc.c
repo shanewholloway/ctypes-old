7ebcf05a89c9da30b9aac9f197a0992ac6f68db8
theller <none@none>
2006-06-30 14:36:21 +0000

    Implement the _ctypes.uses_seh() function which can be used to determine if SEH is used or not.
    --HG--
    branch : ctypes
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk/ctypes%4050456

--- source/callproc.c
+++ source/callproc.c
@@ -1515,7 +1515,21 @@ resize(PyObject *self, PyObject *args)
 	return Py_None;
 }
 
+static PyObject *
+uses_seh(PyObject *self, PyObject *args)
+{
+#ifdef DONT_USE_SEH
+	Py_INCREF(Py_False);
+	return Py_False;
+#else
+	Py_INCREF(Py_True);
+	return Py_True;
+#endif
+}
+
 PyMethodDef module_methods[] = {
+	{"uses_seh", uses_seh, METH_NOARGS,
+	 "Return whether ctypes uses Windows structured exception handling"},
 	{"resize", resize, METH_VARARGS, "Resize the memory buffer of a ctypes instance"},
 #ifdef CTYPES_UNICODE
 	{"set_conversion_mode", set_conversion_mode, METH_VARARGS, set_conversion_mode_doc},
