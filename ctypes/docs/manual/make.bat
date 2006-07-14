5bedad91c1c87c8c5350e618ff7d0ea35325cec7
thomas.heller <none@none>
2006-07-14 18:49:27 +0000

    Another slight reorganization of the ctypes documentation.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4050651

--- ctypes/docs/manual/make.bat
+++ ctypes/docs/manual/make.bat
@@ -1,2 +1,4 @@
+rem Build the ctypes html pages
+c:\python24\scripts\rst2html.py index.txt > index.html
 filter.py ctypes_ref.txt | c:\python24\python c:\python24\scripts\rst2html.py > reference.html
 filter.py ctypes_tut.txt | c:\python24\python c:\python24\scripts\rst2html.py > tutorial.html
