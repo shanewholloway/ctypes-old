76fdf6075dff1a4cca46a3e2f593c540c30a8bfb
thomas.heller <none@none>
2006-08-18 14:43:54 +0000

    util.find_library on Windows simply returns the passed-in name.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4051380

--- ctypes/ctypes/util.py
+++ ctypes/ctypes/util.py
@@ -26,18 +26,7 @@ if os.name == "ce":
     # - ROM dll files (where are they?)
     # - OEM specified search path: HKLM\Loader\SystemPath
     def find_library(name):
-        # Does it make sense to implement this correctly?  For now we
-        # fake.
-        #
-        # The proper solution (also for windows) would be to call
-        # LoadLibraryEx(name, NULL, LOAD_LIBRARY_AS_DATAFILE), then
-        # get the module name with GetModuleFileName and return that.
-        #
-        # OTOH, why use find_library on windows at all? LoadLibrary
-        # does all that's needed.
-        if not "." in name:
-            name += ".dll"
-        return os.path.exists(name)
+        return name
 
 if os.name == "posix" and sys.platform == "darwin":
     from ctypes.macholib.dyld import dyld_find as _dyld_find
