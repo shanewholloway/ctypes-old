092e31dbf92e58967559286cde120df57bd0699b
thomas.heller <none@none>
2006-08-25 06:16:19 +0000

    Forgt to commit this for win64 support.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4051602

--- ctypes/source/callproc.c
+++ ctypes/source/callproc.c
@@ -988,7 +988,11 @@ PyObject *_CallProc(PPROC pProc,
 	}
 	for (i = 0; i < argcount; ++i) {
 		atypes[i] = args[i].ffi_type;
-		if (atypes[i]->type == FFI_TYPE_STRUCT)
+		if (atypes[i]->type == FFI_TYPE_STRUCT 
+#ifdef _WIN64
+		    && atypes[i]->size <= sizeof(void *)
+#endif
+		    )
 			avalues[i] = (void *)args[i].value.p;
 		else
 			avalues[i] = (void *)&args[i].value;
