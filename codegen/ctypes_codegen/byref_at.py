06d1b6859510797d8bf3637e5f57097a5a1383e4
thomas.heller <none@none>
2006-10-31 19:21:22 +0000

    Try to hack a byref_at() function.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4052565

--- codegen/ctypes_codegen/byref_at.py
+++ codegen/ctypes_codegen/byref_at.py
@@ -11,8 +11,7 @@ def _determine_layout():
     result = set()
     for obj in (c_int(), c_longlong(), c_float(), c_double(), (c_int * 32)()):
         ref = byref(obj)
-        result.add((c_byte * 32).from_address(id(ref))[:].index(ord("P")))
-        print (c_void_p * 32).from_address(id(ref))[:].index(id(obj)) * sizeof(c_void_p)
+        result.add((c_void_p * 32).from_address(id(ref))[:].index(id(obj)) * sizeof(c_void_p))
     if len(result) != 1:
         raise RuntimeError, "cannot determine byref() object layout"
     return result.pop()
