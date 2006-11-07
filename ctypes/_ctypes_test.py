96a066da9d4325100578dfa4287c555d4fc3aca0
thomas.heller <none@none>
2006-11-07 07:57:07 +0000

    Fix the _ctypes.py and _ctypes_test.py loader modules to work independend from the cwd.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4052639

--- ctypes/_ctypes_test.py
+++ ctypes/_ctypes_test.py
@@ -4,7 +4,7 @@ def _boot():
     from distutils.util import get_platform
 
     plat_specifier = ".%s-%s" % (get_platform(), sys.version[0:3])
-    build_dir = os.path.join('build', 'lib' + plat_specifier)
+    build_dir = os.path.join(os.path.dirname(__file__), 'build', 'lib' + plat_specifier)
 
     file, path, descr = imp.find_module(__name__, [build_dir])
     imp.load_module(__name__, file, path, descr)
