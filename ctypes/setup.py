627d1223ef872976903f5169aabd2541d04d98ae
thomas.heller <none@none>
2007-03-13 20:25:50 +0000

    Change the ctypes version number in the setup.py script to 1.0.2, this is also the version number in the sources.
    --HG--
    extra : convert_revision : svn%3A6015fed2-1504-0410-9fe1-9d1591cc4771/ctypes/trunk%4054355

--- ctypes/setup.py
+++ ctypes/setup.py
@@ -11,7 +11,7 @@ dlls. It allows wrapping these libraries in pure Python.
 
 LIBFFI_SOURCES='source/libffi'
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 ################################################################
 
